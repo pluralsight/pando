5471e6d8be675dc7279ebc9fb76a48406c65d188
Casey Baggz <caseybaggz@gmail.com>
2022-11-17 18:56:55 +0000

    Deploy website - based on c5264aa0e7c4b0824501cf884e02371e1f9443a3

---
+++ assets/js/0fd7bf17.c9b556fc.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[154],{26716:function(e){e.exports=JSON.parse('{"label":"tabular","permalink":"/docs/next/tags/tabular","allTagsPath":"/docs/next/tags","count":1,"items":[{"id":"design/components/table","title":"Table","description":"Description","permalink":"/docs/next/design/components/table"}]}')}}]);
