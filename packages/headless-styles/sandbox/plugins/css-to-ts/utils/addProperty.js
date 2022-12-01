28ab54146e9d2089e4fb9ad6d41f9dd3f6905c1e
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2022-12-01 10:49:33 -0500

    feat(headless-styles): use deep merge in addProperty helper

--- packages/headless-styles/sandbox/plugins/css-to-ts/utils/addProperty.js
+++ packages/headless-styles/sandbox/plugins/css-to-ts/utils/addProperty.js
@@ -1,7 +1,9 @@
-const addProperty = (obj, key, value) => {
+import deepMerge from './deepMerge'
+
+function addProperty(obj, key, value) {
   const retObj = obj
   if (retObj[key]) {
-    retObj[key] = { ...retObj[key], ...value }
+    retObj[key] = deepMerge(retObj[key], value)
   } else {
     retObj[key] = value
   }
