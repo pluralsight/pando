737602484101b60860b8c21075e57dc3cb56cc74
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2023-03-03 12:08:59 -0500

    test(headless-styles): add missing test cases to pagination JS tests

--- packages/headless-styles/tests/pagination/paginationJS.test.ts
+++ packages/headless-styles/tests/pagination/paginationJS.test.ts
@@ -6,4 +6,22 @@ describe('getJSPaginationProps', () => {
     expect(props?.container.cssProps).toContain('display: flex')
     expect(props?.container.styles.display).toEqual('flex')
   })
+
+  test('should accept a "more" kind', () => {
+    const props = getJSPaginationProps({ kind: 'more' })
+    expect(props?.container.cssProps).toContain('justify-content: center')
+    expect(props?.container.styles.justifyContent).toEqual('center')
+  })
+
+  test('should accept a "newerOlder" kind', () => {
+    const props = getJSPaginationProps({ kind: 'newerOlder' })
+    expect(props?.container.cssProps).toContain('justify-content: center')
+    expect(props?.container.styles.justifyContent).toEqual('center')
+  })
+
+  test('should accept a "bar" kind', () => {
+    const props = getJSPaginationProps({ kind: 'bar' })
+    expect(props?.container.cssProps).toContain('justify-content: flex-end')
+    expect(props?.container.styles.justifyContent).toEqual('flex-end')
+  })
 })
