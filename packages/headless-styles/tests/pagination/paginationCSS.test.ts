e92932b9cdffec0a2583a091f77b0e17ee0c32f0
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2023-03-03 11:47:59 -0500

    test(headless-styles): update pagination tests with new kind option

--- packages/headless-styles/tests/pagination/paginationCSS.test.ts
+++ packages/headless-styles/tests/pagination/paginationCSS.test.ts
@@ -4,11 +4,7 @@ describe('Pagination CSS', () => {
   const baseClass = 'ps-pagination'
   const result = {
     container: {
-      className: `${baseClass} paginationContainer`,
-      style: {
-        gridTemplateColumns: 'repeat(1, 1fr)',
-        textAlign: 'center',
-      },
+      className: `${baseClass} morePaginationContainer`,
     },
     newer: {
       className: `${baseClass}-newer paginationNewer`,
@@ -23,5 +19,29 @@ describe('Pagination CSS', () => {
 
   test('should allow no props to be passed in', () => {
     expect(getPaginationProps()).toEqual(result)
+  })
+
+  test('should accept a "more" kind', () => {
+    expect(getPaginationProps({ kind: 'more' })).toEqual(result)
+  })
+
+  test('should accept a "newerOlder" kind', () => {
+    expect(getPaginationProps({ kind: 'newerOlder' })).toEqual({
+      ...result,
+      container: {
+        ...result.container,
+        className: `${baseClass} newerOlderPaginationContainer`,
+      },
+    })
+  })
+
+  test('should accept a "bar" kind', () => {
+    expect(getPaginationProps({ kind: 'bar' })).toEqual({
+      ...result,
+      container: {
+        ...result.container,
+        className: `${baseClass} barPaginationContainer`,
+      },
+    })
   })
 })
--- packages/headless-styles/tests/pagination/paginationJS.test.ts
+++ packages/headless-styles/tests/pagination/paginationJS.test.ts
@@ -3,7 +3,7 @@ import { getJSPaginationProps } from '../../src'
 describe('getJSPaginationProps', () => {
   test('should accept no options', () => {
     const props = getJSPaginationProps()
-    expect(props?.container.cssProps).toContain('display: grid')
-    expect(props?.container.styles.display).toEqual('grid')
+    expect(props?.container.cssProps).toContain('display: flex')
+    expect(props?.container.styles.display).toEqual('flex')
   })
 })
