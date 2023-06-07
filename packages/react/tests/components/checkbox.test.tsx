a7775d6e73682b647008afc02e3557bc4f0ef1ba
Casey Baggz <caseybaggz@gmail.com>
2023-06-07 08:58:33 -0500

    feat(react): add Checkbox.tsx

--- packages/react/tests/components/checkbox.test.tsx
+++ packages/react/tests/components/checkbox.test.tsx
@@ -1,5 +1,6 @@
+import { createRef } from 'react'
 import { render, screen } from 'test-utils'
-import { Checkbox, FormControlProvider } from '@react'
+import { Checkbox, FormControlProvider, Label } from '@react'
 
 describe('Checkbox', () => {
   it('renders', () => {
@@ -9,5 +10,84 @@ describe('Checkbox', () => {
       </FormControlProvider>
     )
     expect(screen.getByRole('checkbox')).toBeInTheDocument()
+  })
+
+  it('renders with label', () => {
+    render(
+      <FormControlProvider readOnly={true}>
+        <Checkbox id="test" name="test">
+          <Label htmlFor="test">Test</Label>
+        </Checkbox>
+      </FormControlProvider>
+    )
+    expect(screen.getByText(/test/i)).toBeInTheDocument()
+  })
+
+  it('renders with ref', () => {
+    const ref = createRef<HTMLInputElement>()
+    render(
+      <FormControlProvider readOnly={true}>
+        <Checkbox id="test" name="test" ref={ref} />
+      </FormControlProvider>
+    )
+    expect(ref.current).toBeInTheDocument()
+  })
+
+  it('renders with indeterminate', () => {
+    render(
+      <FormControlProvider readOnly={true}>
+        <Checkbox id="test" name="test" indeterminate />
+      </FormControlProvider>
+    )
+    expect(screen.getByRole('checkbox')).toHaveAttribute(
+      'indeterminate',
+      'true'
+    )
+  })
+
+  it('renders with checked', () => {
+    render(
+      <FormControlProvider readOnly={true}>
+        <Checkbox id="test" name="test" checked />
+      </FormControlProvider>
+    )
+    expect(screen.getByRole('checkbox')).toBeChecked()
+  })
+
+  it('renders with disabled', () => {
+    render(
+      <FormControlProvider disabled={true} readOnly={true}>
+        <Checkbox id="test" name="test" />
+      </FormControlProvider>
+    )
+    expect(screen.getByRole('checkbox')).toBeDisabled()
+  })
+
+  it('renders with invalid', () => {
+    render(
+      <FormControlProvider invalid={true} readOnly={true}>
+        <Checkbox id="test" name="test" />
+      </FormControlProvider>
+    )
+    expect(screen.getByRole('checkbox')).toHaveAttribute('aria-invalid')
+    expect(screen.getByRole('checkbox')).toBeInvalid()
+  })
+
+  it('renders with required', () => {
+    render(
+      <FormControlProvider required={true} readOnly={true}>
+        <Checkbox id="test" name="test" />
+      </FormControlProvider>
+    )
+    expect(screen.getByRole('checkbox')).toBeRequired()
+  })
+
+  it('renders with readOnly', () => {
+    render(
+      <FormControlProvider readOnly={true}>
+        <Checkbox id="test" name="test" />
+      </FormControlProvider>
+    )
+    expect(screen.getByRole('checkbox')).toHaveAttribute('readonly')
   })
 })
