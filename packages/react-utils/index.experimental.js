b565b96ffb68666c36099ae3a7c8bc35800a1d45
Casey Baggz <caseybaggz@gmail.com>
2023-06-07 11:14:39 -0500

    refactor(react-utils): update public exports

--- packages/react-utils/index.experimental.js
+++ packages/react-utils/index.experimental.js
@@ -1,23 +1,20 @@
-export { getCachedTheme, setCachedTheme } from './src/helpers/themeHelpers'
-
-// hooks
-
-export { useAutoFormatDate } from './src/hooks/useAutoFormatDate'
-export { useEscToClose } from './src/hooks/useEscToClose'
-export { useFocusTrap } from './src/hooks/useFocusTrap'
-export { useIsIndeterminate as unstable_useIsIndeterminate } from './src/hooks/useIsIndeterminate'
-export { useMenuInteraction } from './src/hooks/menu/useMenuInteraction'
-export { usePreloadedImg } from './src/hooks/usePreloadedImg'
-export { useRovingTabIndex } from './src/hooks/useRovingTabIndex'
-export { useSubmenuInteraction } from './src/hooks/menu/useSubmenuInteraction'
-export { useTheme } from './src/hooks/useTheme'
-
-// Tabs
-
-export { TabsProvider } from './src/hooks/useTabs/context'
 export {
+  getCachedTheme,
+  setCachedTheme,
+  // hooks
+  useAutoFormatDate,
+  useEscToClose,
+  useFocusTrap,
+  useIsIndeterminate,
+  useMenuInteraction,
+  usePreloadedImg,
+  useRovingTabIndex,
+  useSubmenuInteraction,
+  useTheme,
+  // Tabs
+  TabsProvider,
   useTabList,
   useTab,
   usePanelList,
   usePanel,
-} from './src/hooks/useTabs/hooks'
+} from './src/index.ts'
