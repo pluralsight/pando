04df362e9e625813369bb0c58063ca1a9b1754dc
Casey Baggz <caseybaggz@gmail.com>
2023-06-16 12:02:24 -0500

    feat(react): make AlertDialog components

--- packages/sandbox/src/components/ConfirmPage.tsx
+++ packages/sandbox/src/components/ConfirmPage.tsx
@@ -1,103 +1,65 @@
-import { useRef } from 'react'
-import {
-  getAlertDialogProps,
-  getAlertDialogHeaderProps,
-  getAlertDialogHeadingProps,
-  getAlertDialogBodyProps,
-  getAlertDialogLabelProps,
-  getAlertDialogInputProps,
-  getAlertDialogFooterProps,
-  getAlertDialogCancelButtonProps,
-  getAlertDialogConfirmButtonProps,
-} from '@pluralsight/headless-styles'
-import { useFocusTrap } from '@pluralsight/react-utils'
-import { Button, Flex } from '@pluralsight/react'
+import { useState } from 'react'
+import { Button, Flex, ConfirmProvider, useConfirm } from '@pluralsight/react'
 
 function ConfirmFeature() {
-  const dialogRef = useRef<HTMLDialogElement>(null)
-  const { onKeyDown } = useFocusTrap(dialogRef)
-  // const [consent, setConsent] = useState<boolean | null>(null)
-  // const [destructiveConsent, setDestructiveConsent] = useState<boolean | null>(
-  //   null
-  // )
+  const confirm = useConfirm()
+  const destConfirm = useConfirm()
+  const [consent, setConsent] = useState<string | null>(null)
+  const [destructiveConsent, setDestructiveConsent] = useState<string | null>(
+    null
+  )
+
+  const bodyId = 'confirm:body'
+  const headingId = 'confirm:heading'
 
   async function handleShowConfirm() {
-    dialogRef.current?.showModal()
-    // try {
-    //   const userResponse = await confirm.show({
-    //     bodyId: 'confirm:body',
-    //     heading: 'Add new user to team?',
-    //     headingId: 'confirm:heading',
-    //     text: 'This will add a new user to the team.',
-    //   })
-    //   setConsent(userResponse)
-    // } catch (error) {
-    //   console.error(error)
-    // }
+    setConsent(null)
+
+    try {
+      const userResponse = await confirm.show({
+        bodyId,
+        heading: 'Add new user to team?',
+        headingId,
+        text: 'This will add a new user to the team.',
+      })
+      setConsent(userResponse.toString())
+    } catch (error) {
+      console.error(error)
+    }
   }
 
   async function handleShowDestructiveConfirm() {
-    dialogRef.current?.showModal()
-    console.log('show destructive confirm')
-    // try {
-    //   const userResponse = await confirm.show({
-    //     bodyId: 'confirm:body',
-    //     heading: 'Add new user to team?',
-    //     headingId: 'confirm:heading',
-    //     text: 'This will add a new user to the team.',
-    //   })
-    //   setConsent(userResponse)
-    // } catch (error) {
-    //   console.error(error)
-    // }
+    setDestructiveConsent(null)
+
+    try {
+      const userResponse = await destConfirm.show({
+        bodyId: 'dest:body',
+        heading: 'Add new user to team?',
+        headingId: 'dest:heading',
+        kind: 'destructive',
+        text: 'This will add a new user to the team.',
+      })
+      setDestructiveConsent(userResponse.toString())
+    } catch (error) {
+      console.error(error)
+    }
   }
 
   return (
     <div>
       <Flex justify="space-between">
-        <Button onClick={handleShowConfirm}>Confirm</Button>
-        <Button onClick={handleShowDestructiveConfirm} sentiment="danger">
-          Destructive confirm
-        </Button>
+        <Flex direction="column">
+          <Button onClick={handleShowConfirm}>Confirm</Button>
+          <small>User consent: {consent}</small>
+        </Flex>
+
+        <Flex direction="column">
+          <Button onClick={handleShowDestructiveConfirm} sentiment="danger">
+            Destructive confirm
+          </Button>
+          <small>User consent: {destructiveConsent}</small>
+        </Flex>
       </Flex>
-
-      <dialog
-        {...getAlertDialogProps({
-          headingId: 'confirm:heading',
-          bodyId: 'confirm:body',
-        })}
-        onKeyDown={onKeyDown}
-        ref={dialogRef}
-      >
-        <header {...getAlertDialogHeaderProps('non-destructive').header}>
-          <h4 {...getAlertDialogHeadingProps('confirm:heading')}>Confirm</h4>
-        </header>
-
-        <p {...getAlertDialogBodyProps('confirm:body')}>
-          This is a non-destructive confirm alert that will not delete anything
-          or do anything destructive.
-        </p>
-
-        <form {...getAlertDialogFooterProps()}>
-          <Flex gap={16} justify="flex-end">
-            <Button
-              {...getAlertDialogCancelButtonProps().btnOptions}
-              type="submit"
-              value="true"
-            >
-              Cancel
-            </Button>
-            <Button
-              {...getAlertDialogConfirmButtonProps('non-destructive')
-                .btnOptions}
-              formMethod="dialog"
-              value="false"
-            >
-              Confirm
-            </Button>
-          </Flex>
-        </form>
-      </dialog>
     </div>
   )
 }
@@ -106,7 +68,10 @@ export default function ConfirmPage() {
   return (
     <div>
       <h2>ConfirmPage</h2>
-      <ConfirmFeature />
+
+      <ConfirmProvider>
+        <ConfirmFeature />
+      </ConfirmProvider>
     </div>
   )
 }
