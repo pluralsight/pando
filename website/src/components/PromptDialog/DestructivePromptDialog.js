bbe1c885556e338d6e042ce42e6d79d89dd5e1fc
Casey Baggz <caseybaggz@gmail.com>
2023-06-21 11:27:00 -0500

    docs(ref): update prompt-dialog.mdx

--- website/src/components/PromptDialog/DestructivePromptDialog.js
+++ website/src/components/PromptDialog/DestructivePromptDialog.js
@@ -1,95 +1,47 @@
-import React, { useCallback, useRef, useState } from 'react'
-import { createPortal } from 'react-dom'
-import { getButtonProps } from '@pluralsight/headless-styles'
-import Container from '../Container/Container'
+import React from 'react'
 import {
-  Alert,
-  AlertBody,
-  AlertActionButton,
-  AlertCancelButton,
-  AlertFooter,
-  AlertHeader,
-  AlertHeading,
-  AlertInput,
-  AlertLabel,
-  AlertText,
-} from '../Alert'
+  unsafe_Button as Button,
+  unsafe_PromptProvider as PromptProvider,
+  unsafe_usePrompt as usePrompt,
+} from '@pluralsight/react'
+import Container from '../Container/Container.js'
 
-const CONFIRM_KEY = 'DELETE'
+function PromptFeature() {
+  const { prompt } = usePrompt()
+  const CONFIRM_KEY = 'DELETE'
 
-function DestructivePromptDialog() {
-  const triggerRef = useRef(null)
-  const [value, setValue] = useState('')
-  const [showAlert, setShowAlert] = useState(false)
+  async function handleOnClick() {
+    try {
+      const validation = await prompt({
+        bodyId: 'destructiveAlert-body',
+        headingId: 'destructiveAlert-heading',
+        heading: 'Delete Channel',
+        kind: 'destructive',
+        text: 'Are you sure you want to delete this channel? This action cannot be undone.',
+        promptValidationKey: CONFIRM_KEY,
+      })
 
-  const handleCloseAlert = useCallback(() => {
-    setShowAlert(false)
-  }, [])
-
-  function handleShowAlert() {
-    setShowAlert(true)
-  }
-
-  function handleOnChange(event) {
-    setValue(event.target.value)
+      if (validation === CONFIRM_KEY) {
+        // do something
+      }
+    } catch (error) {
+      // handle error
+    }
   }
 
   return (
-    <Container>
-      <button
-        {...getButtonProps({ sentiment: 'danger' }).button}
-        onClick={handleShowAlert}
-        ref={triggerRef}
-      >
-        Delete channel
-      </button>
-
-      {showAlert &&
-        createPortal(
-          <Alert
-            bodyId="destructiveAlert-body"
-            headingId="destructiveAlert-heading"
-            id="destructive-alert"
-            onClose={handleCloseAlert}
-            ref={triggerRef}
-          >
-            <AlertHeader kind="destructive">
-              <AlertHeading id="destructiveAlert-heading">
-                Delete channel
-              </AlertHeading>
-            </AlertHeader>
-            <AlertBody id="destructiveAlert-body">
-              <AlertText>
-                This action <strong>cannot be undone</strong> and will
-                permanently delete all the data associated with this.
-              </AlertText>
-              <AlertLabel htmlFor="destructiveAlert-input">
-                Please type <strong>{CONFIRM_KEY}</strong> to confirm.
-              </AlertLabel>
-              <AlertInput
-                confirmKey={CONFIRM_KEY}
-                id="destructiveAlert-input"
-                name="destructive-input"
-                onChange={handleOnChange}
-                value={value}
-              />
-            </AlertBody>
-            <AlertFooter>
-              <AlertCancelButton onClick={handleCloseAlert}>
-                Cancel
-              </AlertCancelButton>
-              <AlertActionButton
-                kind="destructive"
-                disabled={value !== CONFIRM_KEY}
-              >
-                Delete channel
-              </AlertActionButton>
-            </AlertFooter>
-          </Alert>,
-          document.body
-        )}
-    </Container>
+    <Button sentiment="danger" onClick={handleOnClick}>
+      Delete channel
+    </Button>
   )
 }
 
-export default DestructivePromptDialog
+export default function PromptDialog() {
+  return (
+    <Container>
+      <PromptProvider>
+        <PromptFeature />
+      </PromptProvider>
+    </Container>
+  )
+}
