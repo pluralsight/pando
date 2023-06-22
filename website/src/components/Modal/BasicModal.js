f30ed78fd77daa9fdf0be41a5490ae140444628a
Casey Baggz <caseybaggz@gmail.com>
2023-06-22 11:22:35 -0500

    docs: update modal.mdx

--- website/src/components/Modal/BasicModal.js
+++ website/src/components/Modal/BasicModal.js
@@ -1,78 +1,68 @@
-import React, { useCallback, useRef, useState } from 'react'
-import { createPortal } from 'react-dom'
-import { getButtonProps } from '@pluralsight/headless-styles'
-import Container from '../Container/Container'
-import Modal from './Modal'
+import React, { useRef } from 'react'
+import {
+  unsafe_AlertDialog as AlertDialog,
+  unsafe_AlertDialogBody as AlertDialogBody,
+  unsafe_AlertDialogCloseIconButton as AlertDialogCloseIconButton,
+  unsafe_AlertDialogFooter as AlertDialogFooter,
+  unsafe_AlertDialogHeader as AlertDialogHeader,
+  unsafe_AlertDialogHeading as AlertDialogHeading,
+  unsafe_AlertDialogText as AlertDialogText,
+  unsafe_Button as Button,
+  unsafe_Flex as Flex,
+} from '@pluralsight/react'
 
-function LoginForm() {
-  return (
-    <div>
-      <p>
-        Cillum aliquip enim exercitation consequat pariatur anim anim eiusmod
-        enim. Do tempor dolore ullamco duis est tempor reprehenderit aute
-        pariatur. Officia ex ullamco amet exercitation ea incididunt amet
-        proident aliqua excepteur sint velit occaecat non. Proident id tempor
-        dolore excepteur. Pariatur irure commodo occaecat consequat nulla
-        aliquip officia reprehenderit velit. Mollit aliquip ex voluptate veniam
-        veniam amet laborum duis magna nulla amet ex. Minim do ullamco culpa
-        minim veniam sunt incididunt excepteur sit ipsum anim.
-      </p>
-      <p>
-        Velit aliquip ea anim nulla aute est sunt elit velit id. Proident non in
-        quis id nulla excepteur Lorem voluptate excepteur tempor velit pariatur
-        deserunt. Sunt excepteur dolor consectetur veniam do tempor laboris
-        ipsum velit.
-      </p>
-      <p>
-        Voluptate esse et officia eiusmod ullamco ullamco aliquip sit proident
-        adipisicing eiusmod. Consectetur quis tempor velit nulla magna aliquip
-        Lorem laboris commodo. Commodo eiusmod consequat proident ex velit
-        veniam elit dolor ea sint labore. Consequat consectetur labore esse do
-        in voluptate esse culpa do veniam reprehenderit duis tempor. Sunt cillum
-        enim quis veniam. Nostrud Lorem commodo nostrud veniam sit Lorem fugiat
-        et quis do culpa cillum nisi.
-      </p>
-    </div>
-  )
-}
-
-function BasicModal() {
-  const triggerRef = useRef(null)
-  const [showModal, setShowModal] = useState(false)
-
-  const handleCloseModal = useCallback(() => {
-    setShowModal(false)
-  }, [])
+export default function BasicModal() {
+  const dialogRef = useRef(null)
 
   function handleShowModal() {
-    setShowModal(true)
+    dialogRef.current?.showModal()
+  }
+
+  function handleCloseModal(e) {
+    e.preventDefault()
+    dialogRef.current?.close()
   }
 
   return (
-    <Container>
-      <button
-        {...getButtonProps().button}
-        onClick={handleShowModal}
-        ref={triggerRef}
-      >
-        Open Modal
-      </button>
+    <div>
+      <h1>Modal</h1>
+      <Button onClick={handleShowModal}>Show modal</Button>
 
-      {showModal &&
-        createPortal(
-          <Modal
-            bodyId="normalModal-body"
-            body={<LoginForm />}
-            heading="More information about stuff"
-            headingId="normalModal-header"
-            id="normalModal"
-            onClose={handleCloseModal}
-            ref={triggerRef}
-          />,
-          document.body
-        )}
-    </Container>
+      <AlertDialog bodyId="test:body" headingId="test:heading" ref={dialogRef}>
+        <AlertDialogHeader>
+          <AlertDialogHeading>Some Custom Modal</AlertDialogHeading>
+          <AlertDialogCloseIconButton onClick={handleCloseModal} />
+        </AlertDialogHeader>
+
+        <AlertDialogBody id="test:body">
+          <AlertDialogText>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+            temporibus modi officia nulla libero voluptatibus? Deleniti, maxime.
+            Optio laudantium nobis qui, eos repellat explicabo animi sit natus
+            quia quam sequi!
+          </AlertDialogText>
+          <br />
+          <AlertDialogText>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+            temporibus modi officia nulla libero voluptatibus? Deleniti, maxime.
+            Optio laudantium nobis qui, eos repellat explicabo animi sit natus
+            quia quam sequi!
+          </AlertDialogText>
+        </AlertDialogBody>
+
+        <AlertDialogFooter>
+          <form>
+            <Flex gap={16} justify="flex-end">
+              <Button formMethod="dialog" sentiment="default" usage="outline">
+                Cancel
+              </Button>
+              <Button onClick={handleCloseModal} type="submit">
+                OK
+              </Button>
+            </Flex>
+          </form>
+        </AlertDialogFooter>
+      </AlertDialog>
+    </div>
   )
 }
-
-export default BasicModal
