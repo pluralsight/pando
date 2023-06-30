02453e7c6b4419c8834a8f3ab9282fd67a0a7dc8
Casey Baggz <caseybaggz@gmail.com>
2023-06-30 12:17:37 -0500

    feat(react): add a11y interactions to Menu

---
+++ packages/react/src/utils/const.ts
@@ -0,0 +1,7 @@
+export const ESCAPE = 'Escape'
+export const ENTER = 'Enter'
+export const HOME = 'Home'
+export const END = 'End'
+
+export const ARROW_DOWN = 'ArrowDown'
+export const ARROW_UP = 'ArrowUp'
