d90726c2663073d346be17f1a5542f4125a67acc
Casey Baggz <caseybaggz@gmail.com>
2023-07-24 10:11:07 -0500

    docs(ref): add For page and next badge

--- packages/react/src/components/For.tsx
+++ packages/react/src/components/For.tsx
@@ -6,6 +6,21 @@ export interface ForProps<T, U extends JSX.Element> {
   children: (item: T, index: number) => U
 }
 
+/**
+ * Creates ann array of elements from a list
+ *
+ * @param props.each - The list to iterate over
+ * @param props.fallback - An optional fallback to render if the list is empty
+ *
+ * @returns a map callback as its child; if the list is empty, the optional fallback is returned.
+ *
+ * @example
+ * <For each={items} fallback={<div>No items</div>}>
+ *   {(item, index) => <div key={index}>{item}</div>}
+ * </For>
+ *
+ * @description https://design.pluralsight.com/docs/reference/components/for
+ */
 export function For<T, U extends JSX.Element>(props: ForProps<T, U>) {
   const { each, fallback, children } = props
 
