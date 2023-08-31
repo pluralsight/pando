c3596166db8305e91d12003417cb9750a0e46ca0
Casey Baggz <caseybaggz@gmail.com>
2023-08-31 13:14:58 -0500

    feat(react): add PopoverProvider

--- packages/react/src/components/AlertDialog/sections.tsx
+++ packages/react/src/components/AlertDialog/sections.tsx
@@ -20,7 +20,7 @@ import { Show } from '../../index.ts'
 
 // <AlertDialog>
 
-interface AlertDialogProps
+export interface AlertDialogProps
   extends AlertDialogOptions,
     HTMLAttributes<HTMLDialogElement> {
   onClose?: (e: SyntheticEvent<HTMLDialogElement>) => void
@@ -28,14 +28,15 @@ interface AlertDialogProps
 
 function AlertDialogEl(
   props: AlertDialogProps,
-  ref: ForwardedRef<HTMLDialogElement>
+  ref: ForwardedRef<HTMLDialogElement>,
 ) {
-  const { kind, bodyId, headingId, ...nativeProps } = props
+  const { kind, bodyId, headingId, usage, ...nativeProps } = props
   const defaultHeadingId = useId()
   const pandoProps = getAlertDialogProps({
     bodyId: bodyId,
     headingId: headingId ?? defaultHeadingId,
     kind,
+    usage,
   })
   const { onKeyDown } = useFocusTrap(ref as MutableRefObject<HTMLDialogElement>)
 
@@ -46,13 +47,13 @@ function AlertDialogEl(
 
 // <AlertDialogHeader>
 
-interface AlertDialogHeaderProps
+export interface AlertDialogHeaderProps
   extends Pick<AlertDialogOptions, 'kind'>,
     HTMLAttributes<HTMLDivElement> {}
 
 function AlertDialogHeaderEl(
   props: AlertDialogHeaderProps,
-  ref: ForwardedRef<HTMLDivElement>
+  ref: ForwardedRef<HTMLDivElement>,
 ) {
   const { kind, ...nativeProps } = props
   const pandoProps = getAlertDialogHeaderProps(kind ?? 'non-destructive')
@@ -73,14 +74,14 @@ function AlertDialogHeaderEl(
 
 // <AlertDialogBody>
 
-interface AlertDialogBodyProps
+export interface AlertDialogBodyProps
   extends Omit<HTMLAttributes<HTMLDivElement>, 'id'> {
   id: string
 }
 
 function AlertDialogBodyEl(
   props: AlertDialogBodyProps,
-  ref: ForwardedRef<HTMLDivElement>
+  ref: ForwardedRef<HTMLDivElement>,
 ) {
   const pandoProps = getAlertDialogBodyProps(props.id)
   return <div {...props} {...pandoProps} ref={ref} />
@@ -88,11 +89,11 @@ function AlertDialogBodyEl(
 
 // <AlertDialogFooter>
 
-type AlertDialogFooterProps = HTMLAttributes<HTMLDivElement>
+export type AlertDialogFooterProps = HTMLAttributes<HTMLDivElement>
 
 function AlertDialogFooterEl(
   props: AlertDialogFooterProps,
-  ref: ForwardedRef<HTMLDivElement>
+  ref: ForwardedRef<HTMLDivElement>,
 ) {
   const pandoProps = getAlertDialogFooterProps()
   return <footer {...props} {...pandoProps} ref={ref} />
@@ -100,16 +101,46 @@ function AlertDialogFooterEl(
 
 // exports
 
+/**
+ * Alert dialog wrapper component
+ * @param props any prop that can be passed to the underlying HTMLDialogElement
+ * @param ref React ref to the underlying HTMLDialogElement
+ * @param props.onClose callback when the dialog is closed
+ * @returns <dialog> element
+ */
 export const AlertDialog = forwardRef<HTMLDialogElement, AlertDialogProps>(
-  AlertDialogEl
+  AlertDialogEl,
 )
+
+/**
+ * Alert dialog header component
+ * @param props any prop that can be passed to the underlying HTMLDivElement
+ * @param ref React ref to the underlying HTMLDivElement
+ * @param props.kind the type of alert dialog
+ * @returns <header> element
+ */
 export const AlertDialogHeader = forwardRef<
   HTMLDivElement,
   AlertDialogHeaderProps
 >(AlertDialogHeaderEl)
+
+/**
+ * Alert dialog body component
+ * @param props any prop that can be passed to the underlying HTMLDivElement
+ * @param ref React ref to the underlying HTMLDivElement
+ * @param props.id the id of the alert dialog body
+ * @returns <div> element
+ */
 export const AlertDialogBody = forwardRef<HTMLDivElement, AlertDialogBodyProps>(
-  AlertDialogBodyEl
+  AlertDialogBodyEl,
 )
+
+/**
+ * Alert dialog footer component
+ * @param props any prop that can be passed to the underlying HTMLDivElement
+ * @param ref React ref to the underlying HTMLDivElement
+ * @returns <footer> element
+ */
 export const AlertDialogFooter = forwardRef<
   HTMLDivElement,
   AlertDialogFooterProps
--- packages/react/src/context/Menu/types.ts
+++ packages/react/src/context/Menu/types.ts
@@ -1,17 +1,12 @@
-import type { Dispatch, SetStateAction } from 'react'
-import { type UseFloatingReturn } from '@floating-ui/react-dom'
-import type { ARROW_DOWN, ARROW_UP } from '../../utils/const.ts'
+import type {
+  FloatingContextValue,
+  FloatingTriggerKey,
+} from 'context/shared/floating.types.ts'
 
-export interface MenuContextValue {
-  expanded: boolean
+export interface MenuContextValue extends FloatingContextValue {
   menuId: string
-  triggerId: string
-  triggerKey: TriggerKey
-  setExpanded: Dispatch<SetStateAction<boolean>>
-  setTriggerKey: Dispatch<SetStateAction<TriggerKey>>
-  floating: UseFloatingReturn
 }
 
 // types
 
-export type TriggerKey = typeof ARROW_DOWN | typeof ARROW_UP | null
+export type TriggerKey = FloatingTriggerKey
