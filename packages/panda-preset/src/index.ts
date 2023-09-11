d2ffd1834ff455467516417a3dda4572afdcda80
Casey Baggz <caseybaggz@gmail.com>
2023-09-11 11:44:29 -0500

    feat(panda): create base config

--- packages/panda-preset/src/index.ts
+++ packages/panda-preset/src/index.ts
@@ -1,13 +1,23 @@
-import { definePreset } from '@pandacss/dev'
+import { defineConfig, definePreset } from '@pandacss/dev'
 
 // import { conditions } from './conditions'
 // import { utilities } from './utilities'
 // import { patterns } from './patterns'
 
 export const preset = definePreset({
+  // globalCss,
   // conditions,
   // utilities,
   // patterns,
+  // theme
+})
+
+export const config = defineConfig({
+  preflight: true,
+  prefix: 'pando',
+
+  jsxFramework: 'react',
+  jsxFactory: 'pando',
 })
 
 export default preset
