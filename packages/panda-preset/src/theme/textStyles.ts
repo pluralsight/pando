fb6f13c9a7f6147b417d6c8fa4d05470d681cf8e
Casey Baggz <caseybaggz@gmail.com>
2023-09-13 13:50:00 -0500

    feat(preset): add heading textStyles

--- packages/panda-preset/src/theme/textStyles.ts
+++ packages/panda-preset/src/theme/textStyles.ts
@@ -9,10 +9,10 @@ const displayWeight = {
 }
 
 // h4-h6
-// const headingWeight = {
-//   fontVariationSettings: '"wght" 700',
-//   fontWeight: '700',
-// }
+const headingWeight = {
+  fontVariationSettings: '"wght" 700',
+  fontWeight: '700',
+}
 
 // p
 const bodyWeight = {
@@ -32,27 +32,53 @@ function createTextStyle(description: string, options: Record<string, string>) {
 
 export const textStyles = defineTextStyles({
   h1: {
-    ...createTextStyle('The h1 text style - used in page titles', {
+    ...createTextStyle('The h1 text style - used in page headings', {
       fontSize: '3.5rem',
       lineHeight: '110%',
       ...displayWeight,
     }),
   },
   h2: {
-    ...createTextStyle('The h2 text style - used in page titles', {
+    ...createTextStyle('The h2 text style - used in page headings', {
       fontSize: '5xl',
       lineHeight: '125%',
       ...displayWeight,
     }),
   },
   h3: {
-    ...createTextStyle('The h3 text style - used in secondary titles', {
+    ...createTextStyle('The h3 text style - used in secondary headings', {
       fontSize: '2.5rem',
       lineHeight: '125%',
       ...displayWeight,
     }),
   },
-
+  h4: {
+    ...createTextStyle('The h4 text style - used in secondary headings', {
+      fontSize: '2rem',
+      lineHeight: '125%',
+      ...headingWeight,
+    }),
+  },
+  h5: {
+    ...createTextStyle(
+      'The h5 text style - used in headings that are the lower emphasis',
+      {
+        fontSize: '2xl',
+        lineHeight: '125%',
+        ...headingWeight,
+      },
+    ),
+  },
+  h6: {
+    ...createTextStyle(
+      'The h6 text style - used in headings that are the lowest emphasis',
+      {
+        fontSize: 'lg',
+        lineHeight: '150%',
+        ...headingWeight,
+      },
+    ),
+  },
   body: {
     ...createTextStyle('The body text style - used in paragraphs', {
       fontSize: 'md',
