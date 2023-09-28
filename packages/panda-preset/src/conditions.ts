7a8f5c2e7e76dc9b694bc6c54bc3deafe90dbddc
Casey Baggz <caseybaggz@gmail.com>
2023-09-28 09:49:58 -0500

    feat(preset): add globalCss and conditions

--- packages/panda-preset/src/conditions.ts
+++ packages/panda-preset/src/conditions.ts
@@ -1,7 +1,16 @@
 export const conditions = {
   // themes
   inkyBlueTheme: '[data-theme=inkyBlue] &',
+
   // modes
   lightMode: '[data-color-mode=light] &, &.light, .light &',
   darkMode: '[data-color-mode=dark] &, &.dark, .dark &',
+
+  // states
+  modalOpen: '&:is([data-modal-open=true])',
+  positionBottom: '&:is([data-position=bottom])',
+  positionTop: '&:is([data-position=top])',
+  positionLeft: '&:is([data-position=left])',
+  positionRight: '&:is([data-position=right])',
+  screenReaderOnly: '&:is([data-screen-reader-only=true])',
 }
