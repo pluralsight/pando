3fe63bd435938931cbf97fda9789f76c32035926
Casey Baggz <caseybaggz@gmail.com>
2023-09-28 11:33:04 -0500

    feat(sandbox): update layout in App.tsx

--- packages/sandbox/panda.config.ts
+++ packages/sandbox/panda.config.ts
@@ -1,9 +1,11 @@
-import { defineConfig } from '@pandacss/dev'
+import { defineConfig, type Config } from '@pandacss/dev'
+import pandaPreset from '@pandacss/preset-panda'
 import pandoPreset, { pandoConfig } from '@pluralsight/panda-preset'
 
 export default defineConfig({
-  ...pandoConfig,
-  presets: [pandoPreset],
+  ...(pandoConfig as Config),
+
+  presets: [pandaPreset, pandoPreset],
 
   // Where to look for your css declarations
   include: ['./src/**/*.{js,jsx,ts,tsx}'],
