d6f7cc7afc75b50b8d1bdf41306d5e63ebb0f2f7
Casey Baggz <caseybaggz@gmail.com>
2023-10-05 12:27:28 -0500

    refactor(pando): use bun for tests

--- packages/pando/tests/index.test.ts
+++ packages/pando/tests/index.test.ts
@@ -1,9 +1,10 @@
+import { describe, test, expect, spyOn } from 'bun:test'
 import { itWorks } from '@pando'
 
 describe('itWorks', () => {
   test('it works', () => {
-    const logSpy = jest.spyOn(console, 'log')
+    const logSpy = spyOn(console, 'log')
     itWorks()
-    expect(logSpy).toHaveBeenLastCalledWith('it works!')
+    expect(logSpy).toHaveBeenCalled()
   })
 })
