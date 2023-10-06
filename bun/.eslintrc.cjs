05ae8aa4d3b0b59d2612137d4da00039c10644ed
Casey Baggz <caseybaggz@gmail.com>
2023-10-06 11:17:35 -0500

    build: create bun workspace and build:packages cmd

---
+++ bun/.eslintrc.cjs
@@ -0,0 +1,13 @@
+// const OFF = 'off'
+// const WARN = 'warn'
+// const ERROR = 'error'
+
+module.exports = {
+  extends: ['../.eslintrc.cjs', 'plugin:@typescript-eslint/recommended'],
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  env: {
+    node: true,
+    es2021: true,
+  },
+}
