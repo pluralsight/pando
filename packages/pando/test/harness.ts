d46038fc0dbbb3c7c62ad953e4081a671da86363
Kathy Lavoie <kathy-lavoie@katlavo-mbp.lan>
2023-10-10 14:32:53 -0600

    feat(pando cli): test for arg selection

--- packages/pando/test/harness.ts
+++ packages/pando/test/harness.ts
@@ -1,3 +1,10 @@
 import { realpathSync } from 'fs'
 
 export const pandoExe = realpathSync('src/index.mts')
+
+export const actions = {
+  DOWN: '\x1B\x5B\x42',
+  UP: '\x1B\x5B\x41',
+  ENTER: '\x0D',
+  SPACE: '\x20',
+}
--- packages/pando/test/pando.test.ts
+++ packages/pando/test/pando.test.ts
@@ -1,17 +1,50 @@
-import { selectArg } from '../index.mts'
-import { it, expect } from 'bun:test'
+import { it, expect, spyOn, describe, test } from 'bun:test'
 import { realpathSync } from 'fs'
+import { actions } from './harness'
 
 const pandoExe = realpathSync('src/index.mts')
 
-it('should execute the cli with the update arg', () => {
-  const { stdout, exitCode } = Bun.spawnSync({
-    cmd: ['bun', 'run', pandoExe],
-    stdout: 'pipe',
-    stderr: 'pipe',
+describe('general pando cli', () => {
+  test('should prompt the user to select a cli path if no valid args are provided', () => {
+    const { stdout, exitCode } = Bun.spawnSync({
+      cmd: ['bun', 'run', pandoExe],
+      stdout: 'pipe',
+      stderr: 'pipe',
+    })
+    expect(stdout.toString()).toInclude(
+      'Welcome to the Pando CLI! An argument "setup" or "update" is required to \ncontinue. Which would you like to select?',
+    )
+    expect(exitCode).toBe(1)
   })
-  expect(stdout.toString()).toBe(
-    '? Welcome to the Pando CLI! An argument "setup" or "update" is required to \ncontinue. Which would you like to select? (Use arrow keys)\n❯ setup \n   update \u001b[10D\u001b[9C',
-  )
-  expect(exitCode).toBe(1)
+
+  test('should initiate pandoSetup when selected', async () => {
+    const proc = Bun.spawn({
+      cmd: ['bun', 'run', pandoExe],
+      stdout: 'pipe',
+      stderr: 'pipe',
+      stdin: 'pipe',
+    })
+    proc.stdin.write(actions.ENTER)
+    proc.stdin.end()
+
+    const text = await new Response(proc.stdout).text()
+    expect(text).toInclude('Welcome to Pando setup')
+    console.log('text', text)
+  })
+
+  test('should initiate pandoUpdate when selected', async () => {
+    const proc = Bun.spawn({
+      cmd: ['bun', 'run', pandoExe],
+      stdout: 'pipe',
+      stderr: 'pipe',
+      stdin: 'pipe',
+    })
+    proc.stdin.write(actions.DOWN)
+    proc.stdin.write(actions.ENTER)
+    proc.stdin.end()
+
+    const text = await new Response(proc.stdout).text()
+    expect(text).toInclude('Welcome to Pando update')
+    console.log('text', text)
+  })
 })
