6085c0c7c9673b0d74c7fcdfa449844ee7fbc486
Casey Baggz <caseybaggz@gmail.com>
2023-10-16 12:46:24 -0500

    ci: use @actions/core lib

--- tests/panda-preset/utilities.test.ts
+++ tests/panda-preset/utilities.test.ts
@@ -4,7 +4,7 @@ import { utilities } from '@pluralsight/panda-preset'
 describe('utilities', () => {
   test('should have a spacing utility', () => {
     expect(utilities).toHaveProperty('extend.pxi')
-    expect(utilities.extend.pxi.className).toEqual('padding-inline')
+    expect(utilities.extend.pxi.className).toEqual('padding-inline-start-end')
     expect(utilities.extend.pxi.values).toEqual('spacing')
     expect(utilities.extend.pxi.transform(4)).toEqual({
       paddingInlineStart: 4,
