fec5418ba0b67df0938d9f54cc1611312d2fee2c
Casey Baggz <caseybaggz@gmail.com>
2023-11-03 10:31:08 -0500

    chore: kill old sandbox

--- packages/shared/src/utils/helpers.ts
+++
@@ -1,9 +0,0 @@
-// Public
-
-export function warning(condition: boolean, message: string) {
-  const __DEV__ = process.env.NODE_ENV !== 'production'
-
-  if (__DEV__ && !condition) {
-    console.error(message)
-  }
-}
