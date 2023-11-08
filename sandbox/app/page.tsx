06e9b59cd598f6d14861e71f832d6ff19a48194b
Casey Baggz <caseybaggz@gmail.com>
2023-11-08 10:48:52 -0600

    feat(sandbox): create components

--- sandbox/app/page.tsx
+++ sandbox/app/page.tsx
@@ -1,79 +1,16 @@
-import { type PropsWithChildren } from 'react'
-import { grid, gridItem } from '@/styled-system/patterns'
-import Nav from './components/Nav'
-import Console from './components/Console'
-import AppBar from './components/AppBar'
+import { vstack } from '@/styled-system/patterns'
 
-export default function Home(props: PropsWithChildren<{}>) {
+export default function Home() {
   return (
-    <div
-      className={grid({
-        columns: 12,
-        gap: '2',
-        gridTemplateRows: 'auto 1fr auto',
-        h: '100vh',
-        p: '2',
+    <section
+      className={vstack({
+        color: 'neutral.text.100',
+        justify: 'center',
+        minH: 'full',
       })}
     >
-      <div
-        className={gridItem({
-          display: 'flex',
-          colSpan: 12,
-          justifyContent: 'space-between',
-          pxi: '4',
-          py: '2',
-          rowSpan: 1,
-        })}
-      >
-        <AppBar />
-      </div>
-
-      <div
-        className={gridItem({
-          bgColor: 'neutral.surface.200',
-          colSpan: 2,
-          overflowY: 'auto',
-          rowStart: 2,
-          rowEnd: 4,
-          rounded: 'md',
-        })}
-      >
-        <Nav />
-      </div>
-
-      <main
-        className={gridItem({
-          border: '1px solid',
-          borderColor: 'neutral.border.100',
-          colStart: 3,
-          colEnd: 13,
-          overflowY: 'auto',
-          pxi: '4',
-          py: '2',
-          rowStart: 2,
-          rowSpan: 1,
-          rounded: 'md',
-        })}
-      >
-        {props.children}
-      </main>
-
-      <footer
-        className={gridItem({
-          bgColor: 'neutral.surface.initial',
-          color: 'neutral.text.200',
-          colStart: 3,
-          colEnd: 13,
-          h: '12.5rem',
-          p: '4',
-          rowStart: 3,
-          rowSpan: 1,
-          rounded: 'md',
-          textStyle: 'mono-sm',
-        })}
-      >
-        <Console />
-      </footer>
-    </div>
+      <p>Select a navigation item to begin.</p>
+      <p>One day we can add keyboard shortcuts here like VS Code.</p>
+    </section>
   )
 }
