72b96b3ca4a144cabc9a9abfd463f412b9b02f95
Kathy Lavoie <kathy-lavoie@katlavo-mbp.lan>
2023-11-14 13:52:54 -0700

    docs(panda-preset): add descriptions to tokens & utilities

--- packages/panda-preset/src/theme/tokens.ts
+++ packages/panda-preset/src/theme/tokens.ts
@@ -1,26 +1,77 @@
 export const fonts = {
-  display: { value: 'var(--font-pando)' },
-  sans: { value: 'var(--font-pando)' },
-  mono: { value: 'var(--font-pando-mono)' },
-  monoStrong: { value: 'var(--font-pando-mono-strong)' },
+  display: {
+    description: 'Primary font - used for all text styles except code samples',
+    value: 'var(--font-pando)',
+  },
+  sans: {
+    description: 'Primary font - used for all text styles except code samples',
+    value: 'var(--font-pando)',
+  },
+  mono: {
+    description: 'Code font - used for code samples and syntax',
+    value: 'var(--font-pando-mono)',
+  },
+  monoStrong: {
+    description: 'Code font - used for code samples and syntax',
+    value: 'var(--font-pando-mono-strong)',
+  },
 }
 
 export const zIndex = {
-  hide: { value: -1 },
-  base: { value: 0 },
+  hide: {
+    description:
+      'z-index value - used to hide an element by stacking it below others',
+    value: -1,
+  },
+  base: { description: 'z-index default value', value: 0 },
 
   // pseudo-elements, borders, etc.
-  decorator: { value: 50 },
+  decorator: {
+    description: 'z-index value - used to position pseudo-elements and borders',
+    value: 50,
+  },
 
   // elements
-  dropdown: { value: 1000 },
-  sticky: { value: 1200 },
-  banner: { value: 1300 },
-  overlay: { value: 1400 },
-  modal: { value: 1500 },
-  popover: { value: 1600 },
-  toast: { value: 1700 },
-  tooltip: { value: 1800 },
+  dropdown: {
+    description:
+      'z-index value - used to set the z-order of dropdown elements and their children to stack above or below other elements according to their z-index value',
+    value: 1000,
+  },
+  sticky: {
+    description:
+      'z-index value - used to set the z-order for elements that stick to their nearest scrolling ancestor.',
+    value: 1200,
+  },
+  banner: {
+    description:
+      'z-index value - used to set the z-order of banner elements and their children to stack above or below other elements according to their z-index value',
+    value: 1300,
+  },
+  overlay: {
+    description:
+      'z-index value - used to set the z-order of overlay elements and their children to stack above or below other elements according to their z-index value',
+    value: 1400,
+  },
+  modal: {
+    description:
+      'z-index value - used to set the z-order of modal elements and their children to stack above or below other elements according to their z-index value',
+    value: 1500,
+  },
+  popover: {
+    description:
+      'z-index value - used to set the z-order of popover elements and their children to stack above or below other elements according to their z-index value',
+    value: 1600,
+  },
+  toast: {
+    description:
+      'z-index value - used to set the z-order of toast elements and their children to stack above or below other elements according to their z-index value',
+    value: 1700,
+  },
+  tooltip: {
+    description:
+      'z-index value - used to set the z-order of tooltip elements and their children to stack above or below other elements according to their z-index value',
+    value: 1800,
+  },
 }
 
 export const tokens = {
