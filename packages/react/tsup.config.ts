0657a3e3dc751c989566c3f06a96e748fa87117f
Casey Baggz <caseybaggz@gmail.com>
2023-11-29 11:21:02 -0600

    feat(react): add tsup

---
+++ packages/react/tsup.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig, type Options } from 'tsup'
+import { esbuildPluginFilePathExtensions } from 'esbuild-plugin-file-path-extensions'
+
+function modernConfig(options: Options) {
+  return {
+    entry: options.entry,
+    format: ['cjs', 'esm'],
+    target: ['chrome91', 'firefox90', 'edge91', 'safari15', 'ios15', 'opera77'],
+    outDir: 'build/modern',
+    sourcemap: true,
+    clean: true,
+    esbuildPlugins: [esbuildPluginFilePathExtensions({ esmExtension: 'js' })],
+  } as Options
+}
+
+function legacyConfig(options: Options) {
+  return {
+    entry: options.entry,
+    format: ['cjs', 'esm'],
+    target: ['es2020', 'node16'],
+    outDir: 'build/legacy',
+    sourcemap: true,
+    clean: true,
+    esbuildPlugins: [esbuildPluginFilePathExtensions({ esmExtension: 'js' })],
+  } as Options
+}
+
+export default defineConfig([
+  modernConfig({ entry: ['src/**/*.ts', 'src/**/*.tsx'] }),
+  legacyConfig({ entry: ['src/**/*.ts', 'src/**/*.tsx'] }),
+])
