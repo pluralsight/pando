9dab95b1b490f2ae192445d71ada62010223a1aa
Casey Baggz <caseybaggz@gmail.com>
2023-11-30 10:42:42 -0600

    fix(config): use Node mjs files to prevent ESM errors

--- packages/react/tsup.config.ts
+++ packages/react/tsup.config.ts
@@ -1,29 +1,5 @@
-import { defineConfig, type Options } from 'tsup'
-import { esbuildPluginFilePathExtensions } from 'esbuild-plugin-file-path-extensions'
-
-function modernConfig(options: Options) {
-  return {
-    entry: options.entry,
-    format: ['cjs', 'esm'],
-    target: ['chrome91', 'firefox90', 'edge91', 'safari15', 'ios15', 'opera77'],
-    outDir: 'build/modern',
-    sourcemap: true,
-    clean: true,
-    esbuildPlugins: [esbuildPluginFilePathExtensions({ esmExtension: 'js' })],
-  } as unknown as Options
-}
-
-function legacyConfig(options: Options) {
-  return {
-    entry: options.entry,
-    format: ['cjs', 'esm'],
-    target: ['es2020', 'node16'],
-    outDir: 'build/legacy',
-    sourcemap: true,
-    clean: true,
-    esbuildPlugins: [esbuildPluginFilePathExtensions({ esmExtension: 'js' })],
-  } as unknown as Options
-}
+import { defineConfig } from 'tsup'
+import { modernConfig, legacyConfig } from '@pando/configs'
 
 export default defineConfig([
   modernConfig({ entry: ['src/**/*.ts', 'src/**/*.tsx'] }),
