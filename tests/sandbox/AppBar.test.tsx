abbadb2d31e8d57dd7996a9ee65b06d9b89556b1
Kathy Lavoie <kathy-lavoie@pluralsight.com>
2023-12-05 10:41:51 -0700

    test(sandbox): mod appBar tests

--- tests/sandbox/AppBar.test.tsx
+++ tests/sandbox/AppBar.test.tsx
@@ -4,21 +4,41 @@ import AppBar from 'sandbox/app/components/AppBar'
 describe('AppBar', () => {
   test('renders', () => {
     render(<AppBar />)
-    expect(screen.getByTestId('app-bar')).toBeInTheDocument()
-  })
-  test('contains intended string', () => {
-    render(<AppBar />)
+
     expect(screen.getByText('Pando Dev Sandbox')).toBeInTheDocument()
   })
-  test('left nav contains intended links', () => {
+
+  test('contains home link', () => {
     render(<AppBar />)
-    const leftNav = screen.getByTestId('app-bar')
-    expect(leftNav).toContainElement(screen.getByText('Docs'))
+    const homeLink = screen.getAllByRole('link')[0]
+
+    expect(homeLink).toHaveAttribute('href', '/')
+    expect(homeLink).toHaveTextContent('Pando Dev Sandbox')
   })
-  test('right nav contains intended links', () => {
+
+  test('contains docs link', () => {
     render(<AppBar />)
-    const rightNav = screen.getByTestId('app-bar-right-nav')
-    expect(rightNav).toContainElement(screen.getByText('theme'))
-    expect(rightNav).toContainElement(screen.getByText('GH'))
+    const docsLink = screen.getAllByRole('link')[1]
+
+    expect(docsLink).toHaveAttribute('href', 'https://design.pluralsight.com/')
+    expect(docsLink).toHaveTextContent('Docs')
+  })
+
+  test('contains theme button', () => {
+    render(<AppBar />)
+    const themeButton = screen.getByRole('button')
+
+    expect(themeButton).toHaveTextContent('theme')
+  })
+
+  test('contains github Link', () => {
+    render(<AppBar />)
+    const githubLink = screen.getAllByRole('link')[2]
+
+    expect(githubLink).toHaveAttribute(
+      'href',
+      'https://github.com/pluralsight/pando',
+    )
+    expect(githubLink).toHaveTextContent('GH')
   })
 })
