f65913d25e684db4cfe43eaddabcf3bde183f9c2
Kathy Lavoie <kathy-lavoie@pluralsight.com>
2023-12-05 10:02:48 -0700

    test(sandbox): incorporate pr feedback, remove testids

--- tests/sandbox/layout.test.tsx
+++ tests/sandbox/layout.test.tsx
@@ -1,26 +1,10 @@
 import { screen, render } from 'test-utils'
-import Layout from 'sandbox/app/layout'
+import Layout from 'sandbox/app/Layout'
 
-const component = <Layout>Welcome to the Pando Sandbox!</Layout>
+describe('sandbox layout', () => {
+  it('renders', () => {
+    render(<Layout>Welcome to Pando Sandbox!</Layout>)
 
-describe('layout', () => {
-  test('renders', () => {
-    render(component)
-    expect(screen.getByTestId('layout')).toBeInTheDocument()
-  })
-  test('renders children', () => {
-    render(component)
-    expect(
-      screen.getByText('Welcome to the Pando Sandbox!'),
-    ).toBeInTheDocument()
-  })
-  test('includes intended html attributes', () => {
-    render(component)
-    expect(screen.getByTestId('html')).toHaveAttribute('lang', 'en')
-    expect(screen.getByTestId('html')).toHaveAttribute('data-theme', 'inkyBlue')
-    expect(screen.getByTestId('html')).toHaveAttribute(
-      'data-color-mode',
-      'dark',
-    )
+    expect(screen.getByText('Welcome to Pando Sandbox!')).toBeInTheDocument()
   })
 })
