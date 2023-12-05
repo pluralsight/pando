0232e3d4dd9b7aaa4ad7a6e7a69a4a7948375c2e
Kathy Lavoie <kathy-lavoie@pluralsight.com>
2023-12-05 12:41:42 -0700

    test(sandbox): replace it w test

--- tests/sandbox/layout.test.tsx
+++ tests/sandbox/layout.test.tsx
@@ -2,7 +2,7 @@ import { screen, render } from 'test-utils'
 import Layout from 'sandbox/app/layout'
 
 describe('sandbox layout', () => {
-  it('renders', () => {
+  test('renders', () => {
     render(<Layout>Welcome to Pando Sandbox!</Layout>)
 
     expect(screen.getByText('Welcome to Pando Sandbox!')).toBeInTheDocument()
