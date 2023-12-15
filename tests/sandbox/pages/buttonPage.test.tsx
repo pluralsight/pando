347300fe7e0faa70075000b0f31c98f47724cfb7
Casey Baggz <caseybaggz@gmail.com>
2023-12-15 11:08:05 -0600

    chore: change it to test

--- tests/sandbox/pages/buttonPage.test.tsx
+++ tests/sandbox/pages/buttonPage.test.tsx
@@ -2,12 +2,12 @@ import { screen, render } from 'test-utils'
 import ButtonPage from 'sandbox/app/react/button/page'
 
 describe('Button Page', () => {
-  it('renders the buttons', () => {
+  test('renders the buttons', () => {
     render(<ButtonPage />)
     expect(screen.getAllByRole('button')).toHaveLength(37)
   })
 
-  it('renders the button icons', () => {
+  test('renders the button icons', () => {
     render(<ButtonPage />)
     expect(screen.getAllByRole('img')).toHaveLength(4)
   })
