a3ea45d1a41b3ab01291cf783d9d3df362ef07d3
Kathy Lavoie <kathy-lavoie@pluralsight.com>
2023-12-15 08:58:04 -0700

    chore: update workflows for new docs build, add site for new docs deploy

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -3,6 +3,8 @@ import starlight from '@astrojs/starlight'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://pando.pluralsight.com',
+  base: '/docs',
   integrations: [
     starlight({
       title: 'Pando Docs',
