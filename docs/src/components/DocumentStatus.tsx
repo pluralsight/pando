810655a56c79830d49ddb9e8685d5993bbe2b6bb
Kathy Lavoie <kathy-lavoie@pluralsight.com>
2023-12-21 16:15:14 -0700

    feat(docs): document status tables

--- docs/src/components/DocumentStatus.tsx
+++ docs/src/components/DocumentStatus.tsx
@@ -1,12 +1,10 @@
-import { Icon } from '@astrojs/starlight/components'
-
 type DocumentEntry = {
   title: string
   link?: string
-  status: 'done' | 'todo' | 'in-progress'
+  status: 'done' | 'todo' | 'in-progress' | "won't-do"
 }
 
-export const documents: DocumentEntry[] = [
+const generalDocs: DocumentEntry[] = [
   {
     title: 'Design style guide',
     link: 'https://www.figma.com/proto/uJtPfI38D9i8iQg0UGK2E0/Pando-Design-Guidelines?page-id=0%3A1&type=design&node-id=35-5298&viewport=1226%2C-14299%2C0.55&t=M7BKfoKdz6EBQ7ax-1&scaling=min-zoom&starting-point-node-id=6%3A11626',
@@ -92,65 +90,329 @@ export const documents: DocumentEntry[] = [
     status: 'todo',
   },
   {
-    title: '',
-    link: '',
+    title: 'Add Pando to a Website',
+    link: 'https://design.pluralsight.com/docs/learn/get-started/installation/add-to-website',
     status: 'todo',
   },
   {
-    title: '',
-    link: '',
+    title: 'Quick Start',
+    link: 'https://design.pluralsight.com/docs/learn/get-started/quick-start/',
     status: 'todo',
   },
   {
-    title: '',
-    link: '',
+    title: 'Tutorial: StarRating Component',
+    link: 'https://design.pluralsight.com/docs/learn/get-started/quick-start/tutorial-star-rating',
     status: 'todo',
   },
   {
-    title: '',
-    link: '',
+    title: 'Design Patterns',
+    link: 'https://design.pluralsight.com/docs/learn/learn-pando/design-patterns/',
     status: 'todo',
   },
   {
-    title: '',
-    link: '',
+    title: 'Migrating from Classic',
+    link: 'https://design.pluralsight.com/docs/learn/learn-pando/migration',
+    status: 'todo',
+  },
+  {
+    title: 'Using with Next 13',
+    link: 'https://design.pluralsight.com/docs/learn/learn-pando/next13',
+    status: 'todo',
+  },
+  {
+    title: 'Accessibility',
+    link: 'https://design.pluralsight.com/docs/learn/about-pando/a11y',
+    status: 'todo',
+  },
+  {
+    title: 'Versioning Policy',
+    link: 'https://design.pluralsight.com/docs/learn/about-pando/versioning',
+    status: 'todo',
+  },
+  {
+    title: 'Meet the Team',
+    link: 'https://design.pluralsight.com/docs/learn/about-pando/team',
+    status: 'todo',
+  },
+  {
+    title: 'Themes',
+    link: 'https://design.pluralsight.com/docs/reference/general/themes/',
+    status: 'todo',
+  },
+  {
+    title: 'Token List',
+    link: 'https://design.pluralsight.com/docs/reference/general/themes/token-list',
+    status: 'todo',
+  },
+  {
+    title: 'Customizing Themes',
+    link: 'https://design.pluralsight.com/docs/reference/general/themes/customizing',
+    status: 'todo',
+  },
+  {
+    title: 'Icons',
+    link: 'https://design.pluralsight.com/docs/reference/general/icons',
+    status: 'todo',
+  },
+  {
+    title: 'Typography',
+    link: 'https://design.pluralsight.com/docs/reference/general/typography',
+    status: 'todo',
+  },
+  {
+    title: 'Z-Index',
+    link: 'https://design.pluralsight.com/docs/reference/general/zIndex',
     status: 'todo',
   },
 ]
 
-function getIcon(status: string) {
-  switch (status) {
-    case 'done':
-      return <Icon name="approve-check-circle" />
-    case 'in-progress':
-      return <Icon name="pencil" />
-    case 'todo':
-      return <Icon name="list-format" />
-    default:
-      break
-  }
+export const componentDocs: DocumentEntry[] = [
+  {
+    title: 'ActionMenu',
+    link: 'https://design.pluralsight.com/docs/reference/components/action-menu',
+    status: 'todo',
+  },
+  {
+    title: 'Admonition',
+    link: 'https://design.pluralsight.com/docs/reference/components/admonition',
+    status: 'todo',
+  },
+  {
+    title: 'Avatar',
+    link: 'https://design.pluralsight.com/docs/reference/components/avatar',
+    status: 'todo',
+  },
+  {
+    title: 'Badge',
+    link: 'https://design.pluralsight.com/docs/reference/components/badge',
+    status: 'todo',
+  },
+  {
+    title: 'Button',
+    link: 'https://design.pluralsight.com/docs/reference/components/button',
+    status: 'todo',
+  },
+  {
+    title: 'Checkbox',
+    link: 'https://design.pluralsight.com/docs/reference/components/checkbox',
+    status: 'todo',
+  },
+  {
+    title: 'Circular Progress',
+    link: 'https://design.pluralsight.com/docs/reference/components/circular-progress',
+    status: 'todo',
+  },
+  {
+    title: 'Confirm Dialog',
+    link: 'https://design.pluralsight.com/docs/reference/components/confirm-dialog',
+    status: 'todo',
+  },
+  {
+    title: 'Field Messages',
+    link: 'https://design.pluralsight.com/docs/reference/components/field-message',
+    status: 'todo',
+  },
+  {
+    title: 'Fieldset',
+    link: 'https://design.pluralsight.com/docs/reference/components/fieldset',
+    status: 'todo',
+  },
+  {
+    title: 'Flex',
+    link: 'https://design.pluralsight.com/docs/reference/components/flex',
+    status: 'todo',
+  },
+  {
+    title: 'For',
+    link: 'https://design.pluralsight.com/docs/reference/components/for',
+    status: 'todo',
+  },
+  {
+    title: 'Form Control',
+    link: 'https://design.pluralsight.com/docs/reference/components/form-control',
+    status: 'todo',
+  },
+  {
+    title: 'Form Label',
+    link: 'https://design.pluralsight.com/docs/reference/components/form-label',
+    status: 'todo',
+  },
+  {
+    title: 'Grid',
+    link: 'https://design.pluralsight.com/docs/reference/components/grid',
+    status: 'todo',
+  },
+  {
+    title: 'Icon Button',
+    link: 'https://design.pluralsight.com/docs/reference/components/icon-button',
+    status: 'todo',
+  },
+  {
+    title: 'Icon',
+    link: 'https://design.pluralsight.com/docs/reference/components/icon',
+    status: 'todo',
+  },
+  {
+    title: 'Input',
+    link: 'https://design.pluralsight.com/docs/reference/components/input',
+    status: 'todo',
+  },
+  {
+    title: 'Menu',
+    link: 'https://design.pluralsight.com/docs/reference/components/menu',
+    status: 'todo',
+  },
+  {
+    title: 'Modal',
+    link: 'https://design.pluralsight.com/docs/reference/components/modal',
+    status: 'todo',
+  },
+  {
+    title: 'Popover',
+    link: 'https://design.pluralsight.com/docs/reference/components/popover',
+    status: 'todo',
+  },
+  {
+    title: 'Portal',
+    link: 'https://design.pluralsight.com/docs/reference/components/portal',
+    status: 'todo',
+  },
+  {
+    title: 'Progress Bar',
+    link: 'https://design.pluralsight.com/docs/reference/components/progress-bar',
+    status: 'todo',
+  },
+  {
+    title: 'Prompt Dialog',
+    link: 'https://design.pluralsight.com/docs/reference/components/prompt-dialog',
+    status: 'todo',
+  },
+  {
+    title: 'Radio',
+    link: 'https://design.pluralsight.com/docs/reference/components/radio',
+    status: 'todo',
+  },
+  {
+    title: 'Select',
+    link: 'https://design.pluralsight.com/docs/reference/components/select',
+    status: 'todo',
+  },
+  {
+    title: 'Show',
+    link: 'https://design.pluralsight.com/docs/reference/components/show',
+    status: 'todo',
+  },
+  {
+    title: 'Skeleton',
+    link: 'https://design.pluralsight.com/docs/reference/components/skeleton',
+    status: 'todo',
+  },
+  {
+    title: 'Table',
+    link: 'https://design.pluralsight.com/docs/reference/components/table',
+    status: 'todo',
+  },
+  {
+    title: 'Tabs',
+    link: 'https://design.pluralsight.com/docs/reference/components/tabs',
+    status: 'todo',
+  },
+  {
+    title: 'Tag',
+    link: 'https://design.pluralsight.com/docs/reference/components/tag',
+    status: 'todo',
+  },
+  {
+    title: 'Textarea',
+    link: 'https://design.pluralsight.com/docs/reference/components/text-area',
+    status: 'todo',
+  },
+  {
+    title: 'Text Link',
+    link: 'https://design.pluralsight.com/docs/reference/components/text-link',
+    status: 'todo',
+  },
+  {
+    title: 'Toast',
+    link: 'https://design.pluralsight.com/docs/reference/components/toast',
+    status: 'todo',
+  },
+  {
+    title: 'Toggle',
+    link: 'https://design.pluralsight.com/docs/reference/components/toggle',
+    status: 'todo',
+  },
+  {
+    title: 'Tooltip',
+    link: 'https://design.pluralsight.com/docs/reference/components/tooltip',
+    status: 'todo',
+  },
+]
+
+const reactHooks: DocumentEntry[] = [
+  {
+    title: 'useAutoFormatDate',
+    link: 'https://design.pluralsight.com/docs/reference/react-hooks/useAutoFormatDate',
+    status: 'todo',
+  },
+  {
+    title: 'useIsIndeterminate',
+    link: 'https://design.pluralsight.com/docs/reference/react-hooks/useIsIndeterminate',
+    status: 'todo',
+  },
+  {
+    title: 'usePreloadedImage',
+    link: 'https://design.pluralsight.com/docs/reference/react-hooks/usePreloadedImage',
+    status: 'todo',
+  },
+  {
+    title: 'useTheme',
+    link: 'https://design.pluralsight.com/docs/reference/react-hooks/useTheme',
+    status: 'todo',
+  },
+]
+
+function TableContent({ documents }: { documents: DocumentEntry[] }) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Status</th>
+          <th>Reference Link</th>
+        </tr>
+      </thead>
+      <tbody>
+        {documents.map((doc) => {
+          return (
+            <>
+              <tr>
+                <td>{doc.title}</td>
+                <td>{doc.status}</td>
+                <td>
+                  {doc.link && (
+                    <a href={doc.link} rel="noreferrer" target="_blank">
+                      Link
+                    </a>
+                  )}
+                </td>
+              </tr>
+            </>
+          )
+        })}
+      </tbody>
+    </table>
+  )
 }
 
-export function TableContent() {
-  return (
-    <>
-      {documents.map((doc) => {
-        return (
-          <>
-            <tr>
-              <td>{doc.title}</td>
-              <td>{doc.status}</td>
-              <td>
-                {doc.link && (
-                  <a href={doc.link} rel="noreferrer" target="_blank">
-                    Link
-                  </a>
-                )}
-              </td>
-            </tr>
-          </>
-        )
-      })}
-    </>
-  )
+export function GeneralDocs() {
+  return <TableContent documents={generalDocs} />
+}
+
+export function ComponentDocs() {
+  return <TableContent documents={componentDocs} />
+}
+
+export function ReactHooksDocs() {
+  return <TableContent documents={reactHooks} />
 }
