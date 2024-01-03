96144ee83ca9529f22a9e757aadce50736c994e7
Casey Baggz <caseybaggz@gmail.com>
2024-01-03 10:51:57 -0600

    test: add inputPage.test.tsx

---
+++ tests/sandbox/pages/inputPage.test.tsx
@@ -0,0 +1,18 @@
+import { screen, render } from 'test-utils'
+import InputPage from 'sandbox/app/react/input/page'
+
+describe('Input Page', () => {
+  test('renders the inputs', () => {
+    render(<InputPage />)
+    expect(screen.getAllByRole('textbox')).toHaveLength(8)
+  })
+
+  test('renders the input icons', () => {
+    render(<InputPage />)
+    expect(
+      screen.getAllByRole('img', {
+        hidden: true,
+      }),
+    ).toHaveLength(4)
+  })
+})
