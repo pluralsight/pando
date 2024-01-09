69e0fd5e44850b71a71678f00296700c914ad3b5
Kathy Lavoie <kathy-lavoie@pluralsight.com>
2024-01-09 13:54:06 -0700

    chore(docs): clean up linter errors

--- docs/.eslintrc.cjs
+++ docs/.eslintrc.cjs
@@ -4,47 +4,64 @@ const ERROR = 'error'
 
 module.exports = {
   extends: [
-    '../.eslintrc.cjs',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:import/recommended',
-    'plugin:import/typescript',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
+    // '../.eslintrc.cjs',
+    // 'plugin:@typescript-eslint/recommended',
+    // 'plugin:import/recommended',
+    // 'plugin:import/typescript',
+    // 'plugin:react/recommended',
+    // 'plugin:react-hooks/recommended',
+    'plugin:astro/recommended',
     'plugin:jsx-a11y/recommended',
   ],
-  parser: '@typescript-eslint/parser',
   plugins: ['import', 'react', 'jest', 'sonarjs'],
-  rules: {
-    'import/extensions': [ERROR, 'always', { ignorePackages: true }],
-    'import/order': [
-      WARN,
-      {
-        groups: [
-          'builtin',
-          ['external', 'internal'],
-          'parent',
-          ['sibling', 'index'],
-        ],
-      },
-    ],
-    'import/no-unresolved': [
-      ERROR,
-      {
-        ignore: ['^@'],
-      },
-    ],
-    'jest/no-standalone-expect': OFF,
-    'react/jsx-uses-react': OFF,
-    'react/prop-types': OFF,
-    'react/react-in-jsx-scope': OFF,
-    'sonarjs/no-duplicate-string': OFF,
-  },
+  ignorePatterns: ['**/styled-system/*', '**/dist/*'],
+  // rules: {
+  //   'import/extensions': [ERROR, 'always', { ignorePackages: true }],
+  //   'import/order': [
+  //     WARN,
+  //     {
+  //       groups: [
+  //         'builtin',
+  //         ['external', 'internal'],
+  //         'parent',
+  //         ['sibling', 'index'],
+  //       ],
+  //     },
+  //   ],
+  //   'import/no-unresolved': [
+  //     ERROR,
+  //     {
+  //       ignore: ['^@'],
+  //     },
+  //   ],
+  //   'jest/no-standalone-expect': OFF,
+  //   'react/jsx-uses-react': OFF,
+  //   'react/prop-types': OFF,
+  //   'react/react-in-jsx-scope': OFF,
+  //   'sonarjs/no-duplicate-string': OFF,
+  // },
   env: {
     browser: true,
     es2021: true,
     node: true,
   },
   overrides: [
+    {
+      // Define the configuration for `.astro` file.
+      files: ['*.astro'],
+      // Allows Astro components to be parsed.
+      parser: 'astro-eslint-parser',
+      // Parse the script in `.astro` as TypeScript by adding the following configuration.
+      // It's the setting you need when using TypeScript.
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro'],
+      },
+      rules: {
+        // override/add rules settings here, such as:
+        // "astro/no-set-html-directive": "error"
+      },
+    },
     {
       files: ['**/*.ts?(x)'],
       extends: 'plugin:import/typescript',
@@ -63,7 +80,12 @@ module.exports = {
   settings: {
     'import/extensions': ['.ts', '.tsx'],
     'import/external-module-folders': ['node_modules', 'node_modules/@types'],
-    'import/ignore': ['node_modules', '.json$', '.(scss|less|css|styl)$'],
+    'import/ignore': [
+      'node_modules',
+      '.json$',
+      '.(scss|less|css|styl)$',
+      'styled-system',
+    ],
     'import/parsers': {
       '@typescript-eslint/parser': ['.ts', '.tsx'],
     },
--- docs/src/components/DocumentStatus.tsx
+++ docs/src/components/DocumentStatus.tsx
@@ -1,194 +1,199 @@
 type DocumentEntry = {
   title: string
-  status: 'done' | 'todo' | 'in-progress' | "won't-do"
+  status: 'done' | 'todo' | 'in progress' | "won't-do"
   refLink?: string
   docLink?: string
 }
+
+const TODO = 'todo'
+const IN_PROGRESS = 'in progress'
+const DONE = 'done'
+// const WONT_DO = "won't-do"
 
 const generalDocs: DocumentEntry[] = [
   {
     title: 'Design style guide',
     refLink:
       'https://www.figma.com/proto/uJtPfI38D9i8iQg0UGK2E0/Pando-Design-Guidelines?page-id=0%3A1&type=design&node-id=35-5298&viewport=1226%2C-14299%2C0.55&t=M7BKfoKdz6EBQ7ax-1&scaling=min-zoom&starting-point-node-id=6%3A11626',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Color',
     refLink:
       'https://www.figma.com/proto/uJtPfI38D9i8iQg0UGK2E0/Pando-Design-Guidelines?page-id=0%3A1&type=design&node-id=477-15766&viewport=1226%2C-14299%2C0.55&t=M7BKfoKdz6EBQ7ax-1&scaling=min-zoom&starting-point-node-id=6%3A11626',
-    status: 'in-progress',
+    status: IN_PROGRESS,
     docLink: '/explanation/color',
   },
   {
     title: 'Neutral Palette',
     refLink:
       'https://www.figma.com/proto/uJtPfI38D9i8iQg0UGK2E0/Pando-Design-Guidelines?page-id=0%3A1&type=design&node-id=481-19600&viewport=1226%2C-14299%2C0.55&t=M7BKfoKdz6EBQ7ax-1&scaling=min-zoom&starting-point-node-id=6%3A11626',
-    status: 'done',
+    status: DONE,
     docLink: '/reference/colors/neutral-palette',
   },
   {
     title: 'Action Palette',
     refLink:
       'https://www.figma.com/proto/uJtPfI38D9i8iQg0UGK2E0/Pando-Design-Guidelines?page-id=0%3A1&type=design&node-id=481-19174&viewport=1226%2C-14299%2C0.55&t=M7BKfoKdz6EBQ7ax-1&scaling=min-zoom&starting-point-node-id=6%3A11626',
-    status: 'done',
+    status: DONE,
     docLink: '/reference/colors/action-palette',
   },
   {
     title: 'Info Palette',
     refLink:
       'https://www.figma.com/proto/uJtPfI38D9i8iQg0UGK2E0/Pando-Design-Guidelines?page-id=0%3A1&type=design&node-id=481-19105&viewport=1226%2C-14299%2C0.55&t=M7BKfoKdz6EBQ7ax-1&scaling=min-zoom&starting-point-node-id=6%3A11626',
-    status: 'in-progress',
+    status: IN_PROGRESS,
   },
   {
     title: 'Success Palette',
     refLink:
       'https://www.figma.com/proto/uJtPfI38D9i8iQg0UGK2E0/Pando-Design-Guidelines?page-id=0%3A1&type=design&node-id=483-20597&viewport=1226%2C-14299%2C0.55&t=M7BKfoKdz6EBQ7ax-1&scaling=min-zoom&starting-point-node-id=6%3A11626',
-    status: 'in-progress',
+    status: IN_PROGRESS,
   },
   {
     title: 'Warning Palette',
     refLink:
       'https://www.figma.com/proto/uJtPfI38D9i8iQg0UGK2E0/Pando-Design-Guidelines?page-id=0%3A1&type=design&node-id=477-16070&viewport=1226%2C-14299%2C0.55&t=M7BKfoKdz6EBQ7ax-1&scaling=min-zoom&starting-point-node-id=6%3A11626',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Danger Palette',
     refLink:
       'https://www.figma.com/proto/uJtPfI38D9i8iQg0UGK2E0/Pando-Design-Guidelines?page-id=0%3A1&type=design&node-id=483-20797&viewport=1226%2C-14299%2C0.55&t=M7BKfoKdz6EBQ7ax-1&scaling=min-zoom&starting-point-node-id=6%3A11626',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Typography',
     refLink:
       'https://www.figma.com/proto/uJtPfI38D9i8iQg0UGK2E0/Pando-Design-Guidelines?page-id=0%3A1&type=design&node-id=533-15987&viewport=1226%2C-14299%2C0.55&t=M7BKfoKdz6EBQ7ax-1&scaling=min-zoom&starting-point-node-id=6%3A11626',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'FAQ',
     refLink:
       'https://pluralsight.atlassian.net/wiki/spaces/DO/pages/33503150602/Pando+%28Pluralsight+design+system%29+questions+answered',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Priorities List',
     refLink:
       'https://pluralsight.atlassian.net/wiki/spaces/DO/pages/33647100667/Pando+priorities+list',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Figma UI-kit Update Process',
     refLink:
       'https://pluralsight.atlassian.net/wiki/spaces/DO/pages/33725120835/Figma+UI-kit+Update+Process',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: "Figma component do's and don'ts on detaching",
     refLink:
       'https://pluralsight.atlassian.net/wiki/spaces/DO/pages/33828896833/Figma+components+Dos+and+please+don+ts+on+detaching',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Pando design contributions',
     refLink:
       'https://pluralsight.atlassian.net/wiki/spaces/DO/pages/33847967878/Pando+Design+System+-+Design+Contributions',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'PSDS Classic -> Pando Migration',
     refLink:
       'https://pluralsight.atlassian.net/wiki/spaces/DO/pages/33915469841/PSDS+Classic+Pando+migration',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: "Pando Design FAQ's",
     refLink:
       'https://pluralsight.atlassian.net/wiki/spaces/DO/pages/33938833544/Pando+Design+FAQs',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Glossary',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Add Pando to a Website',
     refLink:
       'https://design.pluralsight.com/docs/learn/get-started/installation/add-to-website',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Quick Start',
     refLink:
       'https://design.pluralsight.com/docs/learn/get-started/quick-start/',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Tutorial: StarRating Component',
     refLink:
       'https://design.pluralsight.com/docs/learn/get-started/quick-start/tutorial-star-rating',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Design Patterns',
     refLink:
       'https://design.pluralsight.com/docs/learn/learn-pando/design-patterns/',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Migrating from Classic',
     refLink: 'https://design.pluralsight.com/docs/learn/learn-pando/migration',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Using with Next 13',
     refLink: 'https://design.pluralsight.com/docs/learn/learn-pando/next13',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Accessibility',
     refLink: 'https://design.pluralsight.com/docs/learn/about-pando/a11y',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Versioning Policy',
     refLink: 'https://design.pluralsight.com/docs/learn/about-pando/versioning',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Meet the Team',
     refLink: 'https://design.pluralsight.com/docs/learn/about-pando/team',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Themes',
     refLink: 'https://design.pluralsight.com/docs/reference/general/themes/',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Token List',
     refLink:
       'https://design.pluralsight.com/docs/reference/general/themes/token-list',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Customizing Themes',
     refLink:
       'https://design.pluralsight.com/docs/reference/general/themes/customizing',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Icons',
     refLink: 'https://design.pluralsight.com/docs/reference/general/icons',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Typography',
     refLink: 'https://design.pluralsight.com/docs/reference/general/typography',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Z-Index',
     refLink: 'https://design.pluralsight.com/docs/reference/general/zIndex',
-    status: 'todo',
+    status: TODO,
   },
 ]
 
@@ -197,196 +202,196 @@ export const componentDocs: DocumentEntry[] = [
     title: 'ActionMenu',
     refLink:
       'https://design.pluralsight.com/docs/reference/components/action-menu',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Admonition',
     refLink:
       'https://design.pluralsight.com/docs/reference/components/admonition',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Avatar',
     refLink: 'https://design.pluralsight.com/docs/reference/components/avatar',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Badge',
     refLink: 'https://design.pluralsight.com/docs/reference/components/badge',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Button',
     refLink: 'https://design.pluralsight.com/docs/reference/components/button',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Checkbox',
     refLink:
       'https://design.pluralsight.com/docs/reference/components/checkbox',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Circular Progress',
     refLink:
       'https://design.pluralsight.com/docs/reference/components/circular-progress',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Confirm Dialog',
     refLink:
       'https://design.pluralsight.com/docs/reference/components/confirm-dialog',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Field Messages',
     refLink:
       'https://design.pluralsight.com/docs/reference/components/field-message',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Fieldset',
     refLink:
       'https://design.pluralsight.com/docs/reference/components/fieldset',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Flex',
     refLink: 'https://design.pluralsight.com/docs/reference/components/flex',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'For',
     refLink: 'https://design.pluralsight.com/docs/reference/components/for',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Form Control',
     refLink:
       'https://design.pluralsight.com/docs/reference/components/form-control',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Form Label',
     refLink:
       'https://design.pluralsight.com/docs/reference/components/form-label',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Grid',
     refLink: 'https://design.pluralsight.com/docs/reference/components/grid',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Icon Button',
     refLink:
       'https://design.pluralsight.com/docs/reference/components/icon-button',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Icon',
     refLink: 'https://design.pluralsight.com/docs/reference/components/icon',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Input',
     refLink: 'https://design.pluralsight.com/docs/reference/components/input',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Menu',
     refLink: 'https://design.pluralsight.com/docs/reference/components/menu',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Modal',
     refLink: 'https://design.pluralsight.com/docs/reference/components/modal',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Popover',
     refLink: 'https://design.pluralsight.com/docs/reference/components/popover',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Portal',
     refLink: 'https://design.pluralsight.com/docs/reference/components/portal',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Progress Bar',
     refLink:
       'https://design.pluralsight.com/docs/reference/components/progress-bar',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Prompt Dialog',
     refLink:
       'https://design.pluralsight.com/docs/reference/components/prompt-dialog',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Radio',
     refLink: 'https://design.pluralsight.com/docs/reference/components/radio',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Select',
     refLink: 'https://design.pluralsight.com/docs/reference/components/select',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Show',
     refLink: 'https://design.pluralsight.com/docs/reference/components/show',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Skeleton',
     refLink:
       'https://design.pluralsight.com/docs/reference/components/skeleton',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Table',
     refLink: 'https://design.pluralsight.com/docs/reference/components/table',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Tabs',
     refLink: 'https://design.pluralsight.com/docs/reference/components/tabs',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Tag',
     refLink: 'https://design.pluralsight.com/docs/reference/components/tag',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Textarea',
     refLink:
       'https://design.pluralsight.com/docs/reference/components/text-area',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Text Link',
     refLink:
       'https://design.pluralsight.com/docs/reference/components/text-link',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Toast',
     refLink: 'https://design.pluralsight.com/docs/reference/components/toast',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Toggle',
     refLink: 'https://design.pluralsight.com/docs/reference/components/toggle',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'Tooltip',
     refLink: 'https://design.pluralsight.com/docs/reference/components/tooltip',
-    status: 'todo',
+    status: TODO,
   },
 ]
 
@@ -395,25 +400,25 @@ const reactHooks: DocumentEntry[] = [
     title: 'useAutoFormatDate',
     refLink:
       'https://design.pluralsight.com/docs/reference/react-hooks/useAutoFormatDate',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'useIsIndeterminate',
     refLink:
       'https://design.pluralsight.com/docs/reference/react-hooks/useIsIndeterminate',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'usePreloadedImage',
     refLink:
       'https://design.pluralsight.com/docs/reference/react-hooks/usePreloadedImage',
-    status: 'todo',
+    status: TODO,
   },
   {
     title: 'useTheme',
     refLink:
       'https://design.pluralsight.com/docs/reference/react-hooks/useTheme',
-    status: 'todo',
+    status: TODO,
   },
 ]
 
