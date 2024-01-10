6b02144f92705d7747bf3277488be897367bd0f4
Casey Baggz <caseybaggz@gmail.com>
2024-01-10 14:10:19 -0600

    refactor(react): update IconButton to use recipe

--- packages/react/src/components/IconButton.tsx
+++ packages/react/src/components/IconButton.tsx
@@ -1,37 +1,46 @@
-'use client'
-
-import { forwardRef, type ButtonHTMLAttributes, type ForwardedRef } from 'react'
 import {
-  getIconButtonProps,
-  getIconProps,
-  splitClassNameProp,
-} from '@pluralsight/headless-styles'
-import type { IconButtonOptions } from '@pluralsight/headless-styles/types'
-import type { UsesIconProps } from './shared/types'
+  forwardRef,
+  type ButtonHTMLAttributes,
+  type ForwardedRef,
+  type PropsWithChildren,
+} from 'react'
+import type { Sizes } from '../components/shared/types'
+import type { ButtonPalette } from './Button'
+import { iconButton } from '@/styled-system/recipes'
 
-interface IconButtonProps
-  extends IconButtonOptions,
-    Required<UsesIconProps>,
-    ButtonHTMLAttributes<HTMLButtonElement> {}
+export type IconButtonUsage = 'text' | 'filled'
+export type IconButtonShape = 'square' | 'circle'
+export type IconButtonSize = Exclude<Sizes, 'xl'>
+
+export interface IconButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement> {
+  ariaLabel: string
+  palette?: ButtonPalette
+  usage?: IconButtonUsage
+  shape?: IconButtonShape
+  size?: IconButtonSize
+}
+
+// <IconButton>
 
 function IconButtonEl(
-  props: IconButtonProps,
+  props: PropsWithChildren<IconButtonProps>,
   ref: ForwardedRef<HTMLButtonElement>,
 ) {
-  const { ariaLabel, sentiment, size, usage, icon, ...nativeProps } = props
-  const pandoProps = getIconButtonProps({
-    ariaLabel,
-    classNames: splitClassNameProp(nativeProps.className),
-    disabled: nativeProps.disabled,
-    sentiment,
-    size,
-    usage,
-  })
-  const Icon = icon ?? null
+  const { palette, size, usage, shape, ...nativeProps } = props
 
   return (
-    <button {...nativeProps} {...pandoProps.button} ref={ref}>
-      <Icon {...getIconProps(pandoProps.iconOptions)} />
+    <button
+      className={iconButton({
+        palette,
+        size,
+        usage,
+        shape,
+      })}
+      {...nativeProps}
+      ref={ref}
+    >
+      {props.children}
     </button>
   )
 }
