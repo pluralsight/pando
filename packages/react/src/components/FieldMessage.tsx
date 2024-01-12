1af08b18cc34279d00117c43155f5c48b60ee0f3
Casey Baggz <caseybaggz@gmail.com>
2024-01-12 09:38:32 -0600

    feat(react): create FieldMessage component

--- packages/react/src/components/FieldMessage.tsx
+++ packages/react/src/components/FieldMessage.tsx
@@ -2,36 +2,48 @@
 
 import {
   forwardRef,
-  useId,
+  useMemo,
   type HTMLAttributes,
   type ForwardedRef,
 } from 'react'
-import {
-  getFieldMessageProps,
-  splitClassNameProp,
-} from '@pluralsight/headless-styles'
-import type { FieldMessageOptions } from '@pluralsight/headless-styles/types'
+// import {
+//   getFieldMessageProps,
+//   splitClassNameProp,
+// } from '@pluralsight/headless-styles'
+// import type { FieldMessageOptions } from '@pluralsight/headless-styles/types'
 import { useFormControl } from '../context/FormControl'
+import { cx } from '@/styled-system/css'
+import { fieldMessage } from '@/styled-system/recipes'
 
-interface FieldMessageProps
-  extends FieldMessageOptions,
-    Omit<HTMLAttributes<HTMLParagraphElement>, 'id'> {}
+export type FieldMessageUsage = 'help' | 'invalid'
+export interface FieldMessageProps
+  extends Omit<HTMLAttributes<HTMLParagraphElement>, 'id'> {
+  id: string
+}
 
 function FieldMessageEl(
   props: FieldMessageProps,
   ref: ForwardedRef<HTMLParagraphElement>,
 ) {
-  const { id, ...nativeProps } = props
-  const uid = useId()
+  const { ...nativeProps } = props
   const { invalid } = useFormControl()
-  const pandoProps = getFieldMessageProps({
-    classNames: splitClassNameProp(nativeProps.className),
-    id: id ?? uid,
-  })
+  const pandoClasses = useMemo(() => {
+    return invalid
+      ? fieldMessage({ usage: 'invalid' })
+      : fieldMessage({ usage: 'help' })
+  }, [invalid])
+  // const pandoProps = getFieldMessageProps({
+  //   classNames: splitClassNameProp(nativeProps.className),
+  //   id: id ?? uid,
+  // })
 
-  if (invalid) return null
-
-  return <small {...nativeProps} {...pandoProps} ref={ref} />
+  return (
+    <small
+      {...nativeProps}
+      className={cx(nativeProps.className, pandoClasses)}
+      ref={ref}
+    />
+  )
 }
 
 export const FieldMessage = forwardRef<HTMLParagraphElement, FieldMessageProps>(
