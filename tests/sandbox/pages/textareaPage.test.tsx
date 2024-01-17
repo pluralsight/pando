6822067f70046d27b6825ce1a7bd14aecd819a5a
Casey Baggz <caseybaggz@gmail.com>
2024-01-17 13:51:47 -0600

    test: add textareaPage.test.tsx

---
+++ tests/sandbox/pages/textareaPage.test.tsx
@@ -0,0 +1,9 @@
+import { screen, render } from 'test-utils'
+import TextareaPage from 'sandbox/app/react/textarea/page'
+
+describe('Textarea Page', () => {
+  test('renders the textarea', () => {
+    render(<TextareaPage />)
+    expect(screen.getAllByRole('textbox')).toHaveLength(18)
+  })
+})
