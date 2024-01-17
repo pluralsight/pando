986c03db2ea3a5936b199bd6b3e679e08a773794
Kathy Lavoie <kathy-lavoie@pluralsight.com>
2024-01-17 12:29:23 -0700

    feat(docs): rm useEffect

--- docs/src/components/ThemeSelectButton.tsx
+++ docs/src/components/ThemeSelectButton.tsx
@@ -1,20 +1,11 @@
 import { useTheme } from '@pluralsight/react'
-import { useEffect } from 'react'
 
 export default function ThemeSelectButton() {
-  const { mode, theme, updateMode } = useTheme()
+  const { mode, updateMode } = useTheme()
 
   function handleUpdateMode() {
     updateMode(mode === 'dark' ? 'light' : 'dark')
   }
-
-  useEffect(() => {
-    document.head.dataset.theme = theme
-  }, [theme])
-
-  useEffect(() => {
-    document.head.dataset.colorMode = mode
-  }, [mode])
 
   return (
     <button
