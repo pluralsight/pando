292b0845464b0edf6a632044b51c3d7dbd0bf0b5
Casey Baggz <caseybaggz@gmail.com>
2024-01-22 12:22:25 -0600

    feat: add toast recipe styles

--- packages/panda-preset/src/recipes/shared/helpers.ts
+++ packages/panda-preset/src/recipes/shared/helpers.ts
@@ -2,6 +2,19 @@ import type { Sentiment } from '../../theme/semantic-tokens/types'
 
 export type RecipePalette<K extends Sentiment[]> = K[]
 
+/**
+ * Creates a palette object for a slot recipe
+ *
+ * @param slots - Array of slot names
+ * @param palettes - Array of palette names
+ * @returns Object with the following shape:
+ * {
+ *  [palette]: {
+ *   [slot]: {
+ *   colorPalette: palette
+ *  }
+ * }
+ */
 export function getSlotRecipePalettes<
   const S extends string,
   const P extends Sentiment,
