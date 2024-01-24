b88b4c4d4b9c0ef74a1ff4c02d7764a81a4f64b7
Casey Baggz <caseybaggz@gmail.com>
2024-01-24 12:34:36 -0600

    chore: fix TS error in toast reducer.ts

--- packages/react/src/context/Toast/reducer.ts
+++ packages/react/src/context/Toast/reducer.ts
@@ -32,7 +32,7 @@ export function toastReducer(
         duration: defaultDuration,
         heading: undefined,
         onAction: undefined,
-        sentiment: 'info',
+        palette: 'info',
       }
 
     default:
