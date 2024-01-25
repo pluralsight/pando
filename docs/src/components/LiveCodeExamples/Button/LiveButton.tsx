23d03a9900836f2ef413d8d93b79807e687ab879
Kathy Lavoie <kathy-lavoie@pluralsight.com>
2024-01-25 16:36:21 -0700

    feat(docs): prop name

--- docs/src/components/LiveCodeExamples/Button/LiveButton.tsx
+++ docs/src/components/LiveCodeExamples/Button/LiveButton.tsx
@@ -3,7 +3,11 @@ import { button } from '@/styled-system/recipes'
 import { css, cx } from '@/styled-system/css'
 import LiveCodeEditor from '../LiveCodeEditor'
 
-export default function LiveButton({ str }: { str: string }) {
+export default function LiveButton({
+  componentString,
+}: {
+  componentString: string
+}) {
   const scope = { Button, button, css, cx }
-  return <LiveCodeEditor str={str} component={scope} />
+  return <LiveCodeEditor componentString={componentString} component={scope} />
 }
--- docs/src/components/LiveCodeExamples/LiveCodeEditor.tsx
+++ docs/src/components/LiveCodeExamples/LiveCodeEditor.tsx
@@ -2,15 +2,15 @@ import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live'
 import { css } from '@/styled-system/css'
 
 export default function LiveCodeEditor({
-  str,
+  componentString,
   component,
 }: {
-  str: string
+  componentString: string
   component: Record<string, unknown> | undefined
 }) {
   return (
     <div className={css({ paddingTop: '4', paddingBottom: '4' })}>
-      <LiveProvider scope={component} code={str}>
+      <LiveProvider scope={component} code={componentString}>
         <LiveEditor />
         <LiveError />
         <LivePreview />
