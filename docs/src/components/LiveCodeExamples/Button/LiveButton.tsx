dacaeb83350cb1829508fcb1d098f70d47d4c557
Kathy Lavoie <kathy-lavoie@pluralsight.com>
2024-01-25 16:24:16 -0700

    feat(docs): included recipes

--- docs/src/components/LiveCodeExamples/Button/LiveButton.tsx
+++ docs/src/components/LiveCodeExamples/Button/LiveButton.tsx
@@ -1,8 +1,9 @@
 import { Button } from '@pluralsight/react'
 import { button } from '@/styled-system/recipes'
+import { css, cx } from '@/styled-system/css'
 import LiveCodeEditor from '../LiveCodeEditor'
 
 export default function LiveButton({ str }: { str: string }) {
-  const scope = { Button, button }
+  const scope = { Button, button, css, cx }
   return <LiveCodeEditor str={str} component={scope} />
 }
