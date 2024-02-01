da0407e5e92abbfa701f691ee3badd77d9ea2707
Casey Baggz <caseybaggz@gmail.com>
2024-02-01 12:33:04 -0600

    chore: update icons tsup config

--- packages/icons/tsup.config.js
+++ packages/icons/tsup.config.js
@@ -4,8 +4,10 @@ import { modernConfig, legacyConfig } from '@pando/configs'
 export default defineConfig([
   modernConfig({
     entry: ['generated/jsx-react/*.ts', 'generated/jsx-react/*.tsx'],
+    external: ['react'],
   }),
   legacyConfig({
     entry: ['generated/jsx-react/*.ts', 'generated/jsx-react/*.tsx'],
+    external: ['react'],
   }),
 ])
