66279189852af05d0b055acbe46c68ec8791a63a
Kathy Lavoie <kathy-lavoie@pluralsight.com>
2024-02-02 15:43:11 -0700

    feat(docs): additional hook info

--- docs/src/components/LiveCodeExamples/UseTheme/LiveUseTheme.tsx
+++ docs/src/components/LiveCodeExamples/UseTheme/LiveUseTheme.tsx
@@ -1,7 +1,8 @@
 import { Button, useTheme } from '@pluralsight/react'
 import LiveCodeEditor from '../LiveCodeEditor'
+import { css } from '@/styled-system/css'
 
-const useModeExample = `  
+const modeSwitchExample = `  
   // import { useTheme } from '@pluralsight/react' <- placeholder for example purposes
   // const { mode, updateMode } = useTheme() <- placeholder for example purposes
 
@@ -21,12 +22,63 @@ const useModeExample = `
   )
 `
 
-export default function LiveButton() {
-  const { mode, updateMode } = useTheme()
+const themeSwitchExample = `  
+  // import { useTheme } from '@pluralsight/react' <- placeholder for example purposes
+  // const { theme, updateTheme } = useTheme() <- placeholder for example purposes
 
-  const scope = { Button, useTheme, mode, updateMode }
+  function handleUpdateTheme() {
+    updateTheme(theme === 'inkyBlue' ? 'aDifferentTheme' : 'inkyBlue')
+  }
+
+  render(
+    <>
+      <Button
+        aria-label={
+          mode === 'inkyBlue' ? 'switch off the inkyBlue theme' : 'switch on the inkyBlue theme'
+        }
+        onClick={handleUpdateTheme}
+        className={theme !== 'inkyBlue' && 'use-theme-example-button'}
+        >
+        {theme === 'inkyBlue' ? 'turn off inkyBlue theme' : 'turn on inkyBlue theme'}
+      </Button>
+    </>
+  )
+`
+
+export default function LiveButton() {
+  const { mode, updateMode, theme, updateTheme } = useTheme()
+
+  const scope = {
+    Button,
+    useTheme,
+    mode,
+    updateMode,
+    theme,
+    updateTheme,
+  }
 
   return (
-    <LiveCodeEditor componentString={useModeExample} scope={scope} noInline />
+    <>
+      <LiveCodeEditor
+        componentString={modeSwitchExample}
+        scope={scope}
+        noInline
+      />
+      {theme !== 'inkyBlue' && (
+        <p className={css({ color: '#fff' })}>
+          <span>
+            Pando theme colors will not display properly without the inkyBlue
+            theme.
+          </span>
+          {'  '}You can inspect the html tag in your browser dev tools to see
+          the applied theme as <code>data-theme="{theme}"</code>
+        </p>
+      )}
+      <LiveCodeEditor
+        componentString={themeSwitchExample}
+        scope={scope}
+        noInline
+      />
+    </>
   )
 }
