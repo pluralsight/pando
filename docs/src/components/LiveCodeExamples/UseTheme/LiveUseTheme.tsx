08243ffb04b6062d6881d767ca95d354cb44d860
Kathy Lavoie <kathy-lavoie@pluralsight.com>
2024-02-02 14:08:51 -0700

    feat(docs): live useTheme mode switcher

--- docs/src/components/LiveCodeExamples/UseTheme/LiveUseTheme.tsx
+++ docs/src/components/LiveCodeExamples/UseTheme/LiveUseTheme.tsx
@@ -1,33 +1,32 @@
 import { Button, useTheme } from '@pluralsight/react'
 import LiveCodeEditor from '../LiveCodeEditor'
-export const useThemeExample1 = `
-    import { useTheme } from '@pluralsight/react'
-  const { mode, updateMode } = useTheme()
+
+const useModeExample = `  
+  // import { useTheme } from '@pluralsight/react' <- placeholder for example purposes
+  // const { mode, updateMode } = useTheme() <- placeholder for example purposes
 
   function handleUpdateMode() {
     updateMode(mode === 'dark' ? 'light' : 'dark')
   }
-    return (<Button
+
+  render(
+    <Button
       aria-label={
-        mode == 'dark' ? 'switch to light mode' : 'switch to dark mode'
+        mode === 'dark' ? 'switch to light mode' : 'switch to dark mode'
       }
       onClick={handleUpdateMode}
     >
-      {mode == 'dark' ? '🌞' : '🌚'}
-    </Button>)`
+      {mode == 'dark' ? 'dark mode is active' : 'light mode is active'}
+    </Button>
+  )
+`
 
-export const useThemeExample = `<strong>Hello World!</strong>`
-
-export default function LiveButton({
-  componentString,
-}: {
-  readonly componentString: string
-}) {
+export default function LiveButton() {
   const { mode, updateMode } = useTheme()
 
   const scope = { Button, useTheme, mode, updateMode }
 
   return (
-    <LiveCodeEditor componentString={componentString} scope={scope} noInline />
+    <LiveCodeEditor componentString={useModeExample} scope={scope} noInline />
   )
 }
