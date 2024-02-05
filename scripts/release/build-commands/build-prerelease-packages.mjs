d42a93d36d538bba2e1608ffe843a545fdd13779
Casey Baggz <caseybaggz@gmail.com>
2024-02-05 13:58:30 -0600

    ci: test experimental build fix

--- scripts/release/build-commands/build-prerelease-packages.mjs
+++ scripts/release/build-commands/build-prerelease-packages.mjs
@@ -4,16 +4,29 @@ import { info, error } from '../../theme.mjs'
 
 async function buildPackages(options) {
   const { packagesList } = options
+  const dependantPandoPackages = packagesList.filter(
+    (packageName) => packageName !== 'icons',
+  )
+
+  console.log(info`\n🛠  Building Icons packages locally...`)
+
+  try {
+    const iconsCwd = getLocalPackagePath('icons')
+    await exec('bun run build', { cwd: iconsCwd })
+  } catch (error) {
+    console.error(error('Unable to build icons package.'))
+    throw error
+  }
 
   console.log(info`\n🛠  Building public packages locally...`)
 
   try {
-    await packagesList.forEach(async (packageName) => {
+    await dependantPandoPackages.forEach(async (packageName) => {
       const cwd = getLocalPackagePath(packageName)
       await exec('bun run build', { cwd })
     })
   } catch (err) {
-    console.error(error('Unable to build all packages.'))
+    console.error(error('Unable to build dependant packages.'))
     throw err
   }
 
