5c70e6c2428353765fedff5dea9f6e475e6ae785
Kathy Lavoie <kathy-lavoie@pluralsight.com>
2024-02-07 15:07:03 -0700

    chore(docs): mod imported pando packages to next versions rather than workspace

--- docs/postcss.config.cjs
+++ docs/postcss.config.cjs
@@ -1,3 +1,15 @@
 module.exports = {
-  plugins: [require('@pandacss/dev/postcss')()],
+  plugins: {
+    '@pandacss/dev/postcss': {},
+    'postcss-flexbugs-fixes': {},
+    'postcss-preset-env': {
+      autoprefixer: {
+        flexbox: 'no-2009',
+      },
+      stage: 3,
+      features: {
+        'custom-properties': false,
+      },
+    },
+  },
 }
