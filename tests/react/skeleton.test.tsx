0d6307beb7b10f059d381c6745f485bfda61dd93
Casey Baggz <caseybaggz@gmail.com>
2024-02-20 14:51:57 -0600

    test: add skeleton.test.tsx

--- tests/react/skeleton.test.tsx
+++ tests/react/skeleton.test.tsx
@@ -1,33 +1,30 @@
+import { jest } from '@jest/globals'
 import { render, screen } from 'test-utils'
-import { Skeleton } from '@react'
+import { Skeleton } from '@pluralsight/react'
 
 describe('Skeleton', () => {
   const testID = 'skeleton'
 
-  it('renders successfully', () => {
+  test('renders successfully', () => {
     render(<Skeleton data-testid={testID} />)
     expect(screen.getByTestId(/skeleton/i)).toBeInTheDocument()
   })
 
-  it('renders children', () => {
-    render(
-      <Skeleton>
-        <p>test text.</p>
-      </Skeleton>
-    )
-    expect(screen.getByText(/test text/i)).toBeInTheDocument()
-  })
-
-  it('allows overriding the className', () => {
+  test('allows overriding the className', () => {
     render(<Skeleton data-testid={testID} className="test-class" />)
     expect(screen.getByTestId(/skeleton/i)).toHaveClass('test-class')
   })
 
-  it('allows native div props', () => {
+  test('allows native div props', () => {
     const mockFn = jest.fn()
     render(<Skeleton data-testid={testID} id="test-id" onClick={mockFn} />)
     expect(screen.getByTestId(/skeleton/i)).toHaveAttribute('id', 'test-id')
     screen.getByTestId(/skeleton/i).click()
     expect(mockFn).toHaveBeenCalled()
+  })
+
+  test('shows aria-busy when loading', () => {
+    render(<Skeleton data-testid={testID} />)
+    expect(screen.getByTestId(/skeleton/i)).toHaveAttribute('aria-busy', 'true')
   })
 })
