947fdf40bf82e1fd9f71d7520c288a53095d9a41
Casey Baggz <caseybaggz@gmail.com>
2024-02-21 12:46:04 -0600

    test: add badgePage tests

--- sandbox/app/components/Nav.tsx
+++ sandbox/app/components/Nav.tsx
@@ -1,6 +1,24 @@
 import { css } from '@/styled-system/css'
 import { vstack } from '@/styled-system/patterns'
+import { textLink } from '@/styled-system/recipes'
 import Link from 'next/link'
+
+const REACT_LINKS = [
+  'admonition',
+  'avatar',
+  'badge',
+  'button',
+  'checkbox',
+  'field-message',
+  'icon-button',
+  'input',
+  'label',
+  'radio',
+  'skeleton',
+  'textarea',
+  'text-link',
+  'toast',
+]
 
 export default function Nav() {
   return (
@@ -20,19 +38,11 @@ export default function Nav() {
       >
         react
       </small>
-      <Link href="/react/admonition">Admonition</Link>
-      <Link href="/react/avatar">Avatar</Link>
-      <Link href="/react/button">Button</Link>
-      <Link href="/react/checkbox">Checkbox</Link>
-      <Link href="/react/field-message">Field Message</Link>
-      <Link href="/react/icon-button">Icon Button</Link>
-      <Link href="/react/input">Input</Link>
-      <Link href="/react/label">Label</Link>
-      <Link href="/react/radio">Radio</Link>
-      <Link href="/react/skeleton">Skeleton</Link>
-      <Link href="/react/textarea">Textarea</Link>
-      <Link href="/react/text-link">Text Link</Link>
-      <Link href="/react/toast">Toast</Link>
+      {REACT_LINKS.map((link) => (
+        <Link className={textLink()} href={`/react/${link}`} key={link}>
+          {link.replace('-', ' ')}
+        </Link>
+      ))}
     </nav>
   )
 }
