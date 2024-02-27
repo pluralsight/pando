d769d3605dd527b25e1eaaa6e78accce2b64b63e
Casey Baggz <caseybaggz@gmail.com>
2024-02-27 11:28:11 -0600

    test: add progressBar.test.tsx

--- tests/react/progressBar.test.tsx
+++ tests/react/progressBar.test.tsx
@@ -1,14 +1,15 @@
+import { jest } from '@jest/globals'
 import { render, screen } from 'test-utils'
-import { ProgressBar } from '@react'
+import { ProgressBar } from '@pluralsight/react'
 
 describe('ProgressBar', () => {
   test('should render', () => {
-    render(<ProgressBar ariaLabel="one" />)
+    render(<ProgressBar ariaLabel="one" now={50} />)
     expect(screen.getByRole('progressbar')).toBeInTheDocument()
   })
 
   test('should render with aria-label', () => {
-    render(<ProgressBar ariaLabel="one" />)
+    render(<ProgressBar ariaLabel="one" now={1} />)
     expect(screen.getByRole('progressbar')).toHaveAttribute('aria-label', 'one')
   })
 
@@ -16,23 +17,23 @@ describe('ProgressBar', () => {
     render(<ProgressBar ariaLabel="one" now={50} />)
     expect(screen.getByRole('progressbar')).toHaveAttribute(
       'aria-valuenow',
-      '50'
+      '50',
     )
   })
 
   test('should render with aria-valuemin', () => {
-    render(<ProgressBar ariaLabel="one" min={10} />)
+    render(<ProgressBar ariaLabel="one" min={10} now={10} />)
     expect(screen.getByRole('progressbar')).toHaveAttribute(
       'aria-valuemin',
-      '10'
+      '10',
     )
   })
 
   test('should render with aria-valuemax', () => {
-    render(<ProgressBar ariaLabel="one" max={90} />)
+    render(<ProgressBar ariaLabel="one" max={90} now={50} />)
     expect(screen.getByRole('progressbar')).toHaveAttribute(
       'aria-valuemax',
-      '90'
+      '90',
     )
   })
 
@@ -44,7 +45,8 @@ describe('ProgressBar', () => {
         ariaLabel="one"
         id="hi"
         onClick={mockFn}
-      />
+        now={50}
+      />,
     )
     expect(screen.getByTestId(/wrapper/i)).toHaveAttribute('id', 'hi')
     screen.getByRole('progressbar').click()
