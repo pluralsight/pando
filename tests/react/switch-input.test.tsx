10d6e4a13319f550d86746cb31ca11cb1a289156
caseybaggz <caseybaggz@gmail.com>
2024-03-05 09:47:01 -0600

    test: migrate react switch-input.test.tsx

--- tests/react/switch-input.test.tsx
+++ tests/react/switch-input.test.tsx
@@ -1,45 +1,61 @@
-import { ChangeEvent, createRef, useState } from 'react'
+import { type ChangeEvent, createRef, useState } from 'react'
 import { render, screen, userEvent } from 'test-utils'
-import { Toggle, ToggleButton, FormControlProvider, Label } from '@react'
+import {
+  SwitchInput,
+  SwitchInputButton,
+  FormControlProvider,
+  SwitchInputLabel,
+} from '@pluralsight/react'
 
-describe('Toggle', () => {
+describe('SwitchInput', () => {
   it('forwards ref', () => {
-    const ref = createRef<HTMLInputElement>()
-    render(<Toggle ref={ref} />)
+    const ref = createRef<HTMLLabelElement>()
+    render(<SwitchInput htmlFor="test" ref={ref} />, {
+      wrapper: FormControlProvider,
+    })
     expect(ref.current).not.toBeNull()
   })
 
   it('renders', () => {
-    render(<Toggle>test</Toggle>)
+    render(<SwitchInput htmlFor="test-1">test</SwitchInput>, {
+      wrapper: FormControlProvider,
+    })
     expect(screen.getByText('test')).toBeInTheDocument()
   })
 
   it('allows a custom className', () => {
-    render(<Toggle className="test">test class</Toggle>)
+    render(
+      <SwitchInput htmlFor="test-2" className="test">
+        test class
+      </SwitchInput>,
+      {
+        wrapper: FormControlProvider,
+      },
+    )
     expect(screen.getByText(/test class/i)).toHaveClass('test')
   })
 
   it('renders children', () => {
     render(
       <FormControlProvider readOnly={true}>
-        <Toggle>
-          <Label htmlFor="test">test</Label>
-          <ToggleButton id="test" name="test" />
-        </Toggle>
-      </FormControlProvider>
+        <SwitchInput htmlFor="test">
+          <SwitchInputLabel>test</SwitchInputLabel>
+          <SwitchInputButton id="test" name="test" />
+        </SwitchInput>
+      </FormControlProvider>,
     )
 
     expect(screen.getByText(/test/i)).toBeInTheDocument()
   })
 })
 
-describe('ToggleButton', () => {
+describe('SwitchInputButton', () => {
   it('forwards ref', () => {
     const ref = createRef<HTMLInputElement>()
     render(
       <FormControlProvider readOnly={true}>
-        <ToggleButton id="test" name="test" ref={ref} />
-      </FormControlProvider>
+        <SwitchInputButton id="test" name="test" ref={ref} />
+      </FormControlProvider>,
     )
     expect(ref.current).not.toBeNull()
   })
@@ -47,8 +63,8 @@ describe('ToggleButton', () => {
   it('renders', () => {
     render(
       <FormControlProvider readOnly={true}>
-        <ToggleButton id="test" name="test" />
-      </FormControlProvider>
+        <SwitchInputButton id="test" name="test" />
+      </FormControlProvider>,
     )
     expect(screen.getByRole('switch')).toBeInTheDocument()
   })
@@ -56,13 +72,13 @@ describe('ToggleButton', () => {
   it('allows a custom className', () => {
     render(
       <FormControlProvider readOnly={true}>
-        <ToggleButton id="test" name="test" className="test" />
-      </FormControlProvider>
+        <SwitchInputButton id="test" name="test" className="test" />
+      </FormControlProvider>,
     )
     expect(screen.getByRole('switch')).toHaveClass('test')
   })
 
-  it('renders updates the toggle state when clicked', async () => {
+  it('renders updates the SwitchInput state when clicked', async () => {
     const user = userEvent.setup()
 
     function App() {
@@ -74,15 +90,15 @@ describe('ToggleButton', () => {
 
       return (
         <FormControlProvider>
-          <Toggle>
-            <Label htmlFor="test">test</Label>
-            <ToggleButton
+          <SwitchInput htmlFor="test">
+            <SwitchInputLabel>test</SwitchInputLabel>
+            <SwitchInputButton
               checked={checked}
               id="test"
               name="test"
               onChange={handleCheck}
             />
-          </Toggle>
+          </SwitchInput>
         </FormControlProvider>
       )
     }
