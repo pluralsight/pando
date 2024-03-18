3b28c5099b0e0aec546b673f512d7ea1c0a09858
Kathy Lavoie <kathy-lavoie@pluralsight.com>
2024-03-18 15:34:15 -0600

    feat: refactor react component to recipe

--- packages/react/src/components/CircularProgress.tsx
+++ packages/react/src/components/CircularProgress.tsx
@@ -1,40 +1,73 @@
-import { forwardRef, type HTMLAttributes, type ForwardedRef } from 'react'
 import {
-  getCircularProgressProps,
-  splitClassNameProp,
-} from '@pluralsight/headless-styles'
-import type { CircularProgressOptions } from '@pluralsight/headless-styles/types'
+  forwardRef,
+  useMemo,
+  type HTMLAttributes,
+  type ForwardedRef,
+} from 'react'
+import {
+  getBaseCircleProps,
+  getStrokeProps,
+  VIEWBOX,
+} from '../helpers/circularProgress.helpers'
+import { circularProgress } from '@/styled-system/recipes'
+
+interface CircularProgressParams {
+  size: 'sm' | 'md'
+  cycle: 'determinate' | 'indeterminate'
+}
 
 interface CircularProgressProps
-  extends CircularProgressOptions,
+  extends CircularProgressParams,
     HTMLAttributes<HTMLDivElement> {
+  ariaLabel: string
   displayValue?: boolean
+  label?: string
+  valueNow?: number
 }
 
 function CircularProgressEl(
   props: CircularProgressProps,
-  ref: ForwardedRef<HTMLDivElement>
+  ref: ForwardedRef<HTMLDivElement>,
 ) {
-  const { ariaLabel, kind, max, min, now, displayValue, size, ...nativeProps } =
-    props
-  const progress = getCircularProgressProps({
+  const {
     ariaLabel,
-    classNames: splitClassNameProp(props.className),
-    kind,
-    max,
-    min,
-    now,
+    cycle,
+    valueNow,
+    displayValue = true,
     size,
-  })
+    label,
+    ...nativeProps
+  } = props
+
+  const native = { ...nativeProps }
+  console.log('native', native)
+
+  const styles = useMemo(() => {
+    const params: CircularProgressParams = {
+      size: size === 'sm' ? 'sm' : 'md',
+      cycle: cycle === 'indeterminate' ? 'indeterminate' : 'determinate',
+    }
+    return circularProgress(params)
+  }, [size, cycle])
 
   return (
-    <div {...nativeProps} {...progress.containerProps} ref={ref}>
-      <svg {...progress.svgBoxProps}>
-        <circle {...progress.baseCircleProps} />
-        <circle {...progress.nowCircleProps} />
+    <div
+      {...nativeProps}
+      aria-valuenow={valueNow}
+      className={styles.root}
+      aria-label={ariaLabel}
+      ref={ref}
+    >
+      <svg viewBox={VIEWBOX}>
+        <circle {...getBaseCircleProps()} className={styles.baseCircle} />
+        <circle
+          {...getBaseCircleProps()}
+          {...getStrokeProps(valueNow ?? 0)}
+          className={styles.secondaryCircle}
+        />
       </svg>
-      {displayValue && size !== 'xs' && (
-        <span {...progress.labelProps}>{progress.labelProps.value}</span>
+      {displayValue && size !== 'sm' && (
+        <span className={styles.text}>{label}</span>
       )}
     </div>
   )
