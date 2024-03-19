f5d9a82e5cb8bc9dbf6cec4f12d08327e89931b3
Kathy Lavoie <kathy-lavoie@pluralsight.com>
2024-03-19 15:45:34 -0600

    docs: rm unnecessary file

--- docs/src/components/LiveCodeExamples/CircularProgress/LiveCircularProgressExamples.ts
+++
@@ -1,8 +0,0 @@
-export const liveButtonExample = `// import { Button } from '@pluralsight/react' <- for example purposes only
-// import { hstack } from '@/styled-system/patterns' <- for example purposes only
-
-<div className={hstack({ gap: '4' })}>
-  <Button usage="outline">Cancel</Button>
-  <Button>Submit</Button>
-</div>
-`
