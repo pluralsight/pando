13f02f9ea6da5127e5e1348543a1bf804e6479db
Kathy Lavoie <kathy-lavoie@pluralsight.com>
2024-03-21 16:41:30 -0600

    feat: add dialog recipe

--- sandbox/app/react/alert-dialog/page.tsx
+++ sandbox/app/react/alert-dialog/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useMemo, useRef } from 'react'
+import { ForwardedRef, RefObject, useMemo, useRef } from 'react'
 import { PageHeading } from '@/app/components/typography/PageHeading'
 import { css } from '@/styled-system/css'
 import { alertDialog } from '@/styled-system/recipes'
@@ -9,36 +9,49 @@ import { Button } from '@pluralsight/react'
 
 // This is a simplified version of the Admonition component for recipe testing.
 // DO NOT USE THIS COMPONENT AS A CUSTOMIZATION TEMPLATE IN PRODUCTION.
+function AlertDialogRecipe({
+  activeRef,
+  close,
+}: {
+  activeRef: ForwardedRef<HTMLDialogElement>
+  close: () => void
+}) {
+  const styles = useMemo(() => {
+    return alertDialog()
+  }, [])
+  return (
+    <dialog className={styles.root} ref={activeRef}>
+      <header className={styles.header}>Header content</header>
+      <div className={styles.popover}>Body content</div>
+      <footer className={styles.footer}>footer content</footer>
+      <button onClick={close}>Close</button>
+    </dialog>
+  )
+}
 
 export default function AlertDialogPage() {
   const dialogRef = useRef<HTMLDialogElement>(null)
 
-  function handleShowModal() {
-    dialogRef.current?.showModal()
+  function handleShowModal(ref: RefObject<HTMLDialogElement>) {
+    ref.current?.showModal()
   }
 
-  function handleCloseModal() {
-    dialogRef.current?.close()
+  function handleCloseModal(ref: RefObject<HTMLDialogElement>) {
+    ref.current?.close()
   }
-
-  const styles = useMemo(() => {
-    return alertDialog()
-  }, [])
 
   return (
     <>
       <section>
         <PageHeading>Recipe Usage</PageHeading>
-        <Button onClick={handleShowModal}>Show modal</Button>
+        <Button onClick={() => handleShowModal(dialogRef)}>Show modal</Button>
         <div className={vstack({ gap: '2' })}>
-          <div>
-            <dialog className={styles.root} ref={dialogRef}>
-              <header className={styles.header}>Header content</header>
-              <div className={styles.popover}>Body content</div>
-              <footer className={styles.footer}>footer content</footer>
-              <button onClick={handleCloseModal}>Close</button>
-            </dialog>
-          </div>
+          <AlertDialogRecipe
+            close={() => {
+              handleCloseModal(dialogRef)
+            }}
+            activeRef={dialogRef}
+          />
         </div>
       </section>
 
