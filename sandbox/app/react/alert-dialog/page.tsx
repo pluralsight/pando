f9e01998a4a21d042e9649a7663e6642959d232f
Kathy Lavoie <kathy-lavoie@pluralsight.com>
2024-03-21 16:20:41 -0600

    feat: simple dialog implementation with recipe

--- sandbox/app/react/alert-dialog/page.tsx
+++ sandbox/app/react/alert-dialog/page.tsx
@@ -1,3 +1,53 @@
-export default function AlertDialog() {
-  return <>alert dialog</>
+'use client'
+
+import { useMemo, useRef } from 'react'
+import { PageHeading } from '@/app/components/typography/PageHeading'
+import { css } from '@/styled-system/css'
+import { alertDialog } from '@/styled-system/recipes'
+import { vstack } from '@/styled-system/patterns'
+import { Button } from '@pluralsight/react'
+
+// This is a simplified version of the Admonition component for recipe testing.
+// DO NOT USE THIS COMPONENT AS A CUSTOMIZATION TEMPLATE IN PRODUCTION.
+
+export default function AlertDialogPage() {
+  const dialogRef = useRef<HTMLDialogElement>(null)
+
+  function handleShowModal() {
+    dialogRef.current?.showModal()
+  }
+
+  function handleCloseModal() {
+    dialogRef.current?.close()
+  }
+
+  const styles = useMemo(() => {
+    return alertDialog()
+  }, [])
+
+  return (
+    <>
+      <section>
+        <PageHeading>Recipe Usage</PageHeading>
+        <Button onClick={handleShowModal}>Show modal</Button>
+        <div className={vstack({ gap: '2' })}>
+          <div>
+            <dialog className={styles.root} ref={dialogRef}>
+              <header className={styles.header}>Header content</header>
+              <div className={styles.popover}>Body content</div>
+              <footer className={styles.footer}>footer content</footer>
+              <button onClick={handleCloseModal}>Close</button>
+            </dialog>
+          </div>
+        </div>
+      </section>
+
+      <section className={css({ my: '4' })}>
+        <PageHeading>React Usage</PageHeading>
+      </section>
+      <section className={css({ my: '4' })}>
+        <PageHeading>Custom Usage</PageHeading>
+      </section>
+    </>
+  )
 }
